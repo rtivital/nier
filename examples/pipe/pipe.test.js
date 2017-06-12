fc3ea72a8da50816cb165325049dd8907efde6a1
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-12 22:30:59 +0300

    add multiple arguments test

--- examples/pipe/pipe.test.js
+++ examples/pipe/pipe.test.js
@@ -4,15 +4,24 @@ const pipe = require('./pipe');
 const mutiplyBy2 = value => value * 2;
 const add1 = value => value + 1;
 const divideBy3 = value => value / 3;
-
-const piped = pipe(
+const pipeSingleArgument = pipe(
   mutiplyBy2,
   add1,
   divideBy3
 );
 
+const concat = (str1, str2) => `${str1} ${str2}`;
+const upperFirst = str => `${str.charAt(0).toUpperCase()}${str.substring(1)}`;
+const upperLast = str => `${str.slice(0, str.length - 1)}${str.slice(str.length - 1).toUpperCase()}`;
+const pipedDoubleArgument = pipe(
+  concat,
+  upperFirst,
+  upperLast
+);
+
 test('pipe', (t) => {
-  t.equals(piped(1), 1, '1 equals 1');
+  t.equals(pipeSingleArgument(1), 1, 'works with single argument functions');
+  t.equals(pipedDoubleArgument('hello', 'there'), 'Hello therE', 'workds with functions that accept multiple arguments');
 
   t.end();
 });
