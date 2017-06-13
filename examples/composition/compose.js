72f64424089cff3f079b5455638042680fad1346
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-13 16:58:10 +0300

    add compose tests

--- examples/composition/compose.js
+++ examples/composition/compose.js
@@ -1,5 +1,12 @@
 const pipe = require('./pipe');
 
+/**
+ * Performs right-to-left function composition. The rightmost function may have
+ * any arity, the remaining functions will receive only one argument.
+ *
+ * @param {...Function} fns functions to pipe
+ * @return {Function}
+ */
 module.exports = function compose(...fns) {
   if (fns.length === 0) {
     throw new Error('compose requires at least one argument');
--- examples/composition/composition.test.js
+++ examples/composition/composition.test.js
@@ -1,9 +1,15 @@
 const test = require('tape');
 const pipe = require('./pipe');
+const compose = require('./compose');
 
 const mutiplyBy2 = value => value * 2;
 const add1 = value => value + 1;
 const divideBy3 = value => value / 3;
+const concat = (str1, str2) => `${str1} ${str2}`;
+const upperFirst = str => `${str.charAt(0).toUpperCase()}${str.substring(1)}`;
+const upperLast = str => `${str.slice(0, str.length - 1)}${str.slice(str.length - 1).toUpperCase()}`;
+const undefinedToZero = value => (typeof value === 'undefined' ? 0 : value);
+const getUndefined = () => undefined;
 
 const pipeSingleArgument = pipe(
   mutiplyBy2, // will be called first
@@ -11,51 +17,56 @@ const pipeSingleArgument = pipe(
   divideBy3 // will be called last
 ); // equals to –> (value) => mutiplyBy2(add1(divideBy3(value)))
 
-const concat = (str1, str2) => `${str1} ${str2}`;
-const upperFirst = str => `${str.charAt(0).toUpperCase()}${str.substring(1)}`;
-const upperLast = str => `${str.slice(0, str.length - 1)}${str.slice(str.length - 1).toUpperCase()}`;
+const composeSingleArgument = compose(
+  divideBy3, // will be called last
+  add1, // will be called second
+  mutiplyBy2 // will be called first
+); // equals to –> (value) => mutiplyBy2(add1(divideBy3(value)))
 
-const pipedDoubleArgument = pipe(
+const pipeDoubleArgument = pipe(
   concat, // will be called first
   upperFirst, // will be called second
   upperLast // will be called last
 ); // equals to –> (value) => divideBy3(add1(mutiplyBy2(value)))
 
-const undefinedToZero = value => (typeof value === 'undefined' ? 0 : value);
-const getUndefined = () => undefined;
-const pipedWithUndefinedReturn = pipe(
+const composeDoubleArgument = compose(
+  upperLast, // will be called last
+  upperFirst, // will be called second
+  concat // will be called first
+); // equals to –> (value) => divideBy3(add1(mutiplyBy2(value)))
+
+const pipeWithFalsyReturn = pipe(
   getUndefined, // will be called first
   undefinedToZero, // will be called second
   add1 // will be called last
-);
+); // equals to –> () => add1(undefinedToZero(getUndefined()))
+
+const composeWithFalsyReturn = compose(
+  add1, // will be called first
+  undefinedToZero, // will be called second
+  getUndefined // will be called last
+); // equals to –> () => add1(undefinedToZero(getUndefined()))
 
 test('pipe', (t) => {
-  // pipe function should throw an error if it has not received any arguments
-  t.throws(
-    () => pipe(),
-    'throws error if argument is not passed'
-  );
+  const singleArgumentTest = divideBy3(add1(mutiplyBy2(1)));
+  const doubleArgumentTest = upperFirst(upperLast(concat('hello', 'there')));
+  const falsyReturnTest = add1(undefinedToZero(getUndefined()));
 
-  // function created with pipe should work with functions that accept only one argument
-  t.equals(
-    pipeSingleArgument(1),
-    divideBy3(add1(mutiplyBy2(1))),
-    'works with single argument functions'
-  );
+  // pipe and compose functions should throw an error if it has not received any arguments
+  t.throws(() => pipe(), 'pipe throws error if argument is not passed');
+  t.throws(() => compose(), 'compose throws error if argument is not passed');
 
-  // function created with pipe should work with functions that accept multiple arguments
-  t.equals(
-    pipedDoubleArgument('hello', 'there'),
-    upperFirst(upperLast(concat('hello', 'there'))),
-    'works with functions that accept multiple arguments'
-  );
+  // function created with pipe or compose should work with functions that accept only one argument
+  t.equals(pipeSingleArgument(1), singleArgumentTest, 'pipe works with single argument functions');
+  t.equals(composeSingleArgument(1), singleArgumentTest, 'compose works with single argument functions');
+
+  // function created with pipe or compose should work with functions that accept multiple arguments
+  t.equals(pipeDoubleArgument('hello', 'there'), doubleArgumentTest, 'pipe works with functions that accept multiple arguments');
+  t.equals(composeDoubleArgument('hello', 'there'), doubleArgumentTest, 'compose works with functions that accept multiple arguments');
 
   // pipe should be able to handle functions that return falsy values
-  t.equals(
-    pipedWithUndefinedReturn(),
-    add1(undefinedToZero(getUndefined())),
-    'works with function that return falsy values'
-  );
+  t.equals(pipeWithFalsyReturn(), falsyReturnTest, 'pipe works with function that return falsy values');
+  t.equals(composeWithFalsyReturn(), falsyReturnTest, 'compose works with function that return falsy values');
 
   t.end();
 });
