a4147da7bcaaa63e2cdfba434c65dc9b18e9d1c3
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-13 16:05:27 +0300

    add pipe jsdoc

--- examples/pipe/pipe.js
+++ examples/pipe/pipe.js
@@ -1,3 +1,22 @@
+/**
+ * @TODO: rewrite explanation
+ *
+ * Performs left-to-right function composition. The leftmost function may have
+ * any arity, the remaining functions will receive only one argument.
+ *
+ * @param {...Function} fns functions to pipe
+ * @return {Function}
+ *
+ * @example
+ * const mutiplyBy2 = value => value * 2;
+ * const add1 = value => value + 1;
+ * const divideBy3 = value => value / 3;
+ * const calculations = pipe(
+ *   mutiplyBy2,
+ *   add1,
+ *   divideBy3
+ * );
+ */
 module.exports = function pipe(...fns) {
   if (fns.length === 0) {
     throw new Error('pipe requires at least one argument');
