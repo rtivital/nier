fe4d6687f6e5155a6752278dce7f813fb142e7e8
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-13 15:03:09 +0300

    add tests explanations in english

--- examples/pipe/pipe.test.js
+++ examples/pipe/pipe.test.js
@@ -6,54 +6,51 @@ const add1 = value => value + 1;
 const divideBy3 = value => value / 3;
 
 const pipeSingleArgument = pipe(
-  mutiplyBy2, // вызывается первой
-  add1, // вызывается второй
-  divideBy3 // вызывается последней
-); // эквивалентно –> (value) => mutiplyBy2(add1(divideBy3(value)))
+  mutiplyBy2, // will be called first
+  add1, // will be called second
+  divideBy3 // will be called last
+); // equals to –> (value) => mutiplyBy2(add1(divideBy3(value)))
 
 const concat = (str1, str2) => `${str1} ${str2}`;
 const upperFirst = str => `${str.charAt(0).toUpperCase()}${str.substring(1)}`;
 const upperLast = str => `${str.slice(0, str.length - 1)}${str.slice(str.length - 1).toUpperCase()}`;
 
 const pipedDoubleArgument = pipe(
-  concat, // вызывается первой
-  upperFirst, // вызывается второй
-  upperLast // вызывается последней
-); // эквивалентно –> (value) => divideBy3(add1(mutiplyBy2(value)))
+  concat, // will be called first
+  upperFirst, // will be called second
+  upperLast // will be called last
+); // equals to –> (value) => divideBy3(add1(mutiplyBy2(value)))
 
 const undefinedToZero = value => (typeof value === 'undefined' ? 0 : value);
 const getUndefined = () => undefined;
 const pipedWithUndefinedReturn = pipe(
-  getUndefined,
-  undefinedToZero,
-  add1
+  getUndefined, // will be called first
+  undefinedToZero, // will be called second
+  add1 // will be called last
 );
 
 test('pipe', (t) => {
-  // функция pipe должна вернуть ошибку,
-  // если ей не было передано ни одного аргумента
+  // pipe function should throw an error if it has not received any arguments
   t.throws(
     () => pipe(),
     'throws error if argument is not passed'
   );
 
-  // функция, созданная с помощью pipe должна работать с функциями,
-  // принимающими один аргумент
+  // function created with pipe should work with functions that accept only one argument
   t.equals(
     pipeSingleArgument(1),
     divideBy3(add1(mutiplyBy2(1))),
     'works with single argument functions'
   );
 
-  // функция, созданная с помощью pipe должна работать с функциями,
-  // принимающими более одного аргумента
+  // function created with pipe should work with functions that accept multiple arguments
   t.equals(
     pipedDoubleArgument('hello', 'there'),
     upperFirst(upperLast(concat('hello', 'there'))),
     'works with functions that accept multiple arguments'
   );
 
-  // функции, передаваемые в pipe дол
+  // pipe should be able to handle functions that return falsy values
   t.equals(
     pipedWithUndefinedReturn(),
     add1(undefinedToZero(getUndefined())),
