cc796ba9b983cac467dcbec6a6354e7904d6d490
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-14 12:21:05 +0300

    add coplement throwing if received argument is not function

--- src/complement/complement.js
+++ src/complement/complement.js
@@ -1,3 +1,7 @@
 module.exports = function complement(fn) {
+  if (typeof fn !== 'function') {
+    throw new Error('complement received argument that is not function');
+  }
+
   return (...args) => !fn(...args);
 };
--- src/complement/complement.test.js
+++ src/complement/complement.test.js
@@ -6,6 +6,7 @@ const getTrue = () => true;
 const getStringFromParams = value => `${value}`;
 
 test('complement', (t) => {
+  t.throws(() => complement(), 'throws if received argument is not function');
   t.equal(complement(getFalse)(), true, 'complements falsy values');
   t.equal(complement(getTrue)(), false, 'complements truthy values');
   t.equal(complement(getStringFromParams)('hello'), false, 'complements functions with params');
