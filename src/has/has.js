352e751f570f1e701f6d3586f0e67226f01a610f
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-18 17:21:42 +0300

    add new has functionality with path detection

--- src/has/has.js
+++ src/has/has.js
@@ -1,7 +1,30 @@
 const curry = require('../curry/curry');
+const isArray = require('../isArray/isArray');
+const isString = require('../isString/isString');
 
 function has(path, object) {
-  return object != null && Object.prototype.hasOwnProperty.call(object, path);
+  const pathIsString = isString(path);
+  const pathIsArray = isArray(path);
+
+  if (!pathIsArray && !pathIsString) {
+    throw new Error('has received argument that is not array or string');
+  }
+
+  if (object == null) {
+    return false;
+  }
+
+  if (pathIsString) {
+    return Object.prototype.hasOwnProperty.call(object, path);
+  }
+
+  const pathToTest = path[0];
+
+  if (path.length > 1) {
+    return has(path.slice(1), object[pathToTest]);
+  }
+
+  return has(pathToTest, object);
 }
 
 module.exports = curry(has, 2);
--- src/has/has.test.js
+++ src/has/has.test.js
@@ -2,6 +2,7 @@ const test = require('tape-catch');
 const has = require('./has');
 
 const hasHello = has('hello');
+const hasHelloPath = has(['hello', 'there']);
 
 test('has', (t) => {
   t.equals(
@@ -20,6 +21,18 @@ test('has', (t) => {
     hasHello({ there: 'you' }),
     false,
     'detects unexisting path'
+  );
+
+  t.equals(
+    hasHelloPath({ hello: { there: 'you' } }),
+    true,
+    'detects deeply nested existing path'
+  );
+
+  t.equals(
+    hasHelloPath({ hello: { you: 'there' } }),
+    false,
+    'detects deeply nested unexisting path'
   );
 
   t.end();
