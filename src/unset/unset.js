811a05917d3931eb817255d378e6cb76bdf5b8a8
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-18 18:07:31 +0300

    add new unset module

--- src/unset/unset.js
+++ src/unset/unset.js
@@ -1,9 +1,37 @@
 const curry = require('../curry/curry');
+const isString = require('../isString/isString');
+const isArray = require('../isArray/isArray');
+const has = require('../has/has');
+const set = require('../set/set');
 
 function unset(path, object) {
-  const newObject = Object.assign({}, object);
-  delete newObject[path];
-  return newObject;
+  const pathIsString = isString(path);
+  const pathIsArray = isArray(path);
+
+  if (!pathIsString && !pathIsArray) {
+    throw new Error('unset received path that is not an array or string');
+  }
+
+  if (!has(path, object)) {
+    return object;
+  }
+
+  if (pathIsString) {
+    const newObject = Object.assign({}, object);
+    delete newObject[path];
+    return newObject;
+  }
+
+  let finalProp = object;
+  for (let i = 0, l = path.length - 1; i < l; i += 1) {
+    finalProp = finalProp[path[i]];
+  }
+
+  const finalClone = Object.assign({}, finalProp);
+  delete finalClone[path[path.length - 1]];
+
+
+  return set(path.slice(0, path.length - 1), finalClone, object);
 }
 
 module.exports = curry(unset, 2);
--- src/unset/unset.test.js
+++ src/unset/unset.test.js
@@ -1,14 +1,44 @@
 const test = require('tape-catch');
 const unset = require('./unset');
 
-const original = { hello: 'world', hi: 'you' };
+const testObject = {
+  testing: {
+    makes: {
+      you: {
+        feel: 'protected',
+      },
+    },
+  },
+
+  nope: true,
+};
+
+const unsetFeel = unset(['testing', 'makes', 'you', 'feel']);
 
 test('unset', (t) => {
-  t.deepEqual(
-    unset('hello', original),
-    { hi: 'you' },
-    'removes property from object'
+  t.throws(
+    () => unset(0, testObject),
+    'throws if provided path is not array or string'
   );
 
+  t.equal(
+    typeof unsetFeel,
+    'function',
+    'is curried'
+  );
+
+  t.deepEqual(
+    unsetFeel(testObject),
+    {
+      testing: {
+        makes: {
+          you: {},
+        },
+      },
+
+      nope: true,
+    },
+    'deletes deeply nested path from object'
+  );
   t.end();
 });
