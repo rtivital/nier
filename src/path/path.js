d683ea7f60916a8e6f9e8f026651d9f1d48d806d
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-20 12:48:01 +0300

    path refactoring

--- src/path/path.js
+++ src/path/path.js
@@ -7,22 +7,21 @@ function path(pathTo, object) {
   // if path is not an array or string throw TypeError
   validatePath('path', pathTo);
 
-  let pathValue;
-
   if (has(pathTo, object)) {
     if (isString(pathTo)) {
-      pathValue = object[pathTo];
-    } else { // assume that path is an array
-      let acc = object[pathTo[0]];
-      for (let i = 1, l = pathTo.length; i < l; i += 1) {
-        acc = acc[pathTo[i]];
-      }
-
-      pathValue = acc;
+      return object[pathTo];
     }
+
+    // assume that path is an array
+    let acc = object[pathTo[0]];
+    for (let i = 1, l = pathTo.length; i < l; i += 1) {
+      acc = acc[pathTo[i]];
+    }
+
+    return acc;
   }
 
-  return pathValue;
+  return undefined;
 }
 
 module.exports = curry(path, 2);
