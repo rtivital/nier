3cc1219a65893c7cd57e14717d1850bb0d784778
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-20 13:20:32 +0300

    path refactoring

--- src/path/path.js
+++ src/path/path.js
@@ -7,21 +7,21 @@ function path(pathTo, object) {
   // if path is not an array or string throw TypeError
   validatePath('path', pathTo);
 
-  if (has(pathTo, object)) {
-    if (isString(pathTo)) {
-      return object[pathTo];
-    }
-
-    // assume that path is an array
-    let acc = object[pathTo[0]];
-    for (let i = 1, l = pathTo.length; i < l; i += 1) {
-      acc = acc[pathTo[i]];
-    }
-
-    return acc;
+  if (!has(pathTo, object)) {
+    return undefined;
   }
 
-  return undefined;
+  if (isString(pathTo)) {
+    return object[pathTo];
+  }
+
+  // assume that path is an array
+  let acc = object[pathTo[0]];
+  for (let i = 1, l = pathTo.length; i < l; i += 1) {
+    acc = acc[pathTo[i]];
+  }
+
+  return acc;
 }
 
 module.exports = curry(path, 2);
