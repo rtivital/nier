f2a276981507b322b0595889b4a0502debfe60b2
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-28 20:00:52 +0300

    remove pipe and compose docs

--- src/compose/compose.js
+++ src/compose/compose.js
@@ -1,10 +1,3 @@
-/**
- * Performs right-to-left function composition. The rightmost function may have
- * any arity, the remaining functions will receive only one argument.
- *
- * @param {...Function} fns functions to pipe
- * @return {Function}
- */
 module.exports = function compose(...fns) {
   if (fns.length === 0) {
     throw new Error('pipe requires at least one function argument');
--- src/pipe/pipe.js
+++ src/pipe/pipe.js
@@ -1,10 +1,3 @@
-/**
- * Performs left-to-right function composition. The leftmost function may have
- * any arity, the remaining functions will receive only one argument.
- *
- * @param {...Function} fns functions to pipe
- * @return {Function}
- */
 module.exports = function pipe(...fns) {
   if (fns.length === 0) {
     throw new Error('pipe requires at least one function argument');
