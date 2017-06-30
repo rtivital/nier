e287e062eae96d0ee301d61da8b26d93f82ccd81
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-30 12:44:26 +0300

    append module refactoring

--- src/append/append.js
+++ src/append/append.js
@@ -2,13 +2,15 @@ const isArrayLike = require('../isArrayLike/isArrayLike');
 const cloneArrayLike = require('../_internal/cloneArrayLike/cloneArrayLike');
 const curry = require('../curry/curry');
 
-function append(element, array) {
-  if (!isArrayLike(array)) {
-    throw new Error('append received value that is not array or array like');
+const ERROR_MESSAGE = 'N.append received data that is not array or array like structure';
+
+function append(value, data) {
+  if (!isArrayLike(data)) {
+    throw new Error(ERROR_MESSAGE);
   }
 
-  const result = cloneArrayLike(array);
-  result[array.length] = element;
+  const result = cloneArrayLike(data);
+  result[data.length] = value;
 
   return result;
 }
