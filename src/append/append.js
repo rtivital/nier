c62f88fbd29b46c7c55cac66befa94050714e3ab
Vitaly Rtishchev <rtivital@gmail.com>
2017-06-30 12:37:48 +0300

    add new append logic

--- src/append/append.js
+++ src/append/append.js
@@ -1,7 +1,25 @@
 const curry = require('../curry/curry');
+const isArrayLike = require('../isArrayLike/isArrayLike');
+
+function cloneArrayLike(array) {
+  const result = [];
+
+  for (let i = 0, l = array.length; i < l; i += 1) {
+    result[i] = array[i];
+  }
+
+  return result;
+}
 
 function append(element, array) {
-  return [...array, element];
+  if (!isArrayLike(array)) {
+    throw new Error('append received value that is not array or array like');
+  }
+
+  const result = cloneArrayLike(array);
+  result[array.length] = element;
+
+  return result;
 }
 
 module.exports = curry(append);
--- src/append/append.test.js
+++ src/append/append.test.js
@@ -3,17 +3,15 @@ const append = require('./append');
 
 const appendValue = append('value');
 const original = ['hello', 'world'];
+const arrayLike = { 0: 'hello', 1: 'world', length: 2 };
 
 test('append', (t) => {
-  t.equal(
-    typeof appendValue,
-    'function',
-    'produces curried function'
-  );
+  t.equal(typeof appendValue, 'function', 'is curried');
+  t.throws(() => appendValue(null), 'throws if receives non array or array like value');
 
   t.deepEqual(
     appendValue(original),
-    [...original, 'value'],
+    ['hello', 'world', 'value'],
     'appends value to array'
   );
 
@@ -21,6 +19,18 @@ test('append', (t) => {
     original,
     ['hello', 'world'],
     'does not mutate original array'
+  );
+
+  t.deepEqual(
+    appendValue(arrayLike),
+    ['hello', 'world', 'value'],
+    'appends value to array like'
+  );
+
+  t.deepEqual(
+    appendValue('hi'),
+    ['h', 'i', 'value'],
+    'transforms provided array like value to array'
   );
 
   t.end();
