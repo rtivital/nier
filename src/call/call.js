36b9248d2576ab91379945831ca398ba9ba61c95
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-03 19:42:26 +0300

    add call function docs

--- src/call/call.js
+++ src/call/call.js
@@ -1,8 +1,14 @@
 const curry = require('../curry/curry');
 const isFunction = require('../isFunction/isFunction');
 
-// fn не функция
 
+/**
+ * Calls provided function with given arguments.
+ *
+ * @param {Function} fn function to call
+ * @param {...*} args list of arguments to call function with
+ * @return {*} result of calling function with given list of arguments
+ */
 function call(fn, ...args) {
   if (!isFunction(fn)) {
     throw new Error('N.call received first argument which type is not function');
