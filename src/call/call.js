ed06e9e554b8f16ab59f225146d01bff4bd63eda
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-03 19:21:34 +0300

    add call function error handling and new tests

--- src/call/call.js
+++ src/call/call.js
@@ -1,6 +1,13 @@
 const curry = require('../curry/curry');
+const isFunction = require('../isFunction/isFunction');
+
+// fn не функция
 
 function call(fn, ...args) {
+  if (!isFunction(fn)) {
+    throw new Error('N.call received first argument which type is not function');
+  }
+
   return fn(...args);
 }
 
--- src/call/call.test.js
+++ src/call/call.test.js
@@ -2,23 +2,10 @@ const test = require('tape-catch');
 const call = require('./call');
 
 test('call', (t) => {
-  t.equals(
-    typeof call(f => f),
-    'function',
-    'is curried'
-  );
-
-  t.equals(
-    call(f => f, 1),
-    1,
-    'provides single argument to callback function'
-  );
-
-  t.equals(
-    call((a, b) => a + b)(2, 45),
-    47,
-    'provides multiple arguments to callback function'
-  );
+  t.throws(() => call(1, 'hello'), 'throws error if received non-function first argument');
+  t.equals(typeof call(f => f), 'function', 'is curried');
+  t.equals(call(f => f, 1), 1, 'provides single argument to callback function');
+  t.equals(call((a, b) => a + b)(2, 45), 47, 'provides multiple arguments to callback function');
 
   t.end();
 });
