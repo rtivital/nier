e597b7b9f869de7ee5d270210af6dd436cf2829e
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-04 12:04:28 +0300

    add cloneArrayLike module test

---
+++ src/_internal/cloneArrayLike/cloneArrayLike.test.js
@@ -0,0 +1,34 @@
+const test = require('tape-catch');
+const cloneArrayLike = require('./cloneArrayLike');
+
+function cloneArguments() {
+  return cloneArrayLike(arguments); // eslint-disable-line
+}
+
+test('cloneArrayLike', (t) => {
+  t.deepEquals(
+    cloneArrayLike([1, 2, 3]),
+    [1, 2, 3],
+    'clones array'
+  );
+
+  t.deepEquals(
+    cloneArrayLike('nier'),
+    ['n', 'i', 'e', 'r'],
+    'chunks string into array'
+  );
+
+  t.deepEquals(
+    cloneArguments(1, 2, 3),
+    [1, 2, 3],
+    'clones arguments list',
+  );
+
+  t.deepEquals(
+    cloneArrayLike({ 0: 'hello', 1: 'world', length: 2 }),
+    ['hello', 'world'],
+    'transforms array like object to array'
+  );
+
+  t.end();
+});
