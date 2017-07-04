1f44968dedf3c27b4743c0401ce07ef964920dde
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-04 13:19:43 +0300

    add new append tests

--- src/append/append.test.js
+++ src/append/append.test.js
@@ -1,36 +1,20 @@
 const test = require('tape-catch');
 const append = require('./append');
 
-const appendValue = append('value');
-const original = ['hello', 'world'];
-const arrayLike = { 0: 'hello', 1: 'world', length: 2 };
-
 test('append', (t) => {
-  t.equal(typeof appendValue, 'function', 'is curried');
-  t.throws(() => appendValue(null), 'throws if receives non array or array like value');
+  t.equal(typeof append('nier'), 'function', 'is curried');
+  t.throws(() => append('nier')(null), 'throws if receives non array or array like value');
 
   t.deepEqual(
-    appendValue(original),
-    ['hello', 'world', 'value'],
+    append('nier', ['hello', 'world']),
+    ['hello', 'world', 'nier'],
     'appends value to array'
   );
 
   t.deepEqual(
-    original,
-    ['hello', 'world'],
-    'does not mutate original array'
-  );
-
-  t.deepEqual(
-    appendValue(arrayLike),
-    ['hello', 'world', 'value'],
-    'appends value to array like'
-  );
-
-  t.deepEqual(
-    appendValue('hi'),
-    ['h', 'i', 'value'],
-    'transforms provided array like value to array'
+    append('nier', { 0: 'hello', 1: 'world', length: 2 }),
+    ['hello', 'world', 'nier'],
+    'transforms array like value to array and appends value'
   );
 
   t.end();
