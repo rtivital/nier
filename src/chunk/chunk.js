ec17be9ad6c5f6e6d91a6718f1aa4dec4e6c2934
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-04 11:49:35 +0300

    add chunk docs

--- src/chunk/chunk.js
+++ src/chunk/chunk.js
@@ -3,13 +3,22 @@ const cloneArrayLike = require('../_internal/cloneArrayLike/cloneArrayLike');
 const curry = require('../curry/curry');
 const isArrayLike = require('../isArrayLike/isArrayLike');
 
-function chunk(size, array) {
-  if (!isArrayLike(array)) {
+
+/**
+ * Creates an array of elements split into groups with provided length value.
+ * If array can't be split evenly, the final chunk will contain the remaining elements.
+ *
+ * @param {number} size size of each chunk
+ * @param {Array} value value to process
+ * @return {Array} array of chunks
+ */
+function chunk(size, value) {
+  if (!isArrayLike(value)) {
     throw new Error('N.chunk in not able to split values that are not array or array like');
   }
 
   const parsedSize = toInteger(size);
-  const safeClone = cloneArrayLike(array);
+  const safeClone = cloneArrayLike(value);
   const result = [];
 
   for (let i = 0, l = safeClone.length; i < l; i += parsedSize) {
