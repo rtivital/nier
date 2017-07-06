c98e86044aae49c4112e1f63e9fdd537cb12cd67
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-06 18:12:42 +0300

    add primitives equals comparisons

--- src/equals/equals.js
+++ src/equals/equals.js
@@ -2,15 +2,21 @@ const isPrimitive = require('../_internal/isPrimitive/isPrimitive');
 const curry = require('../curry/curry');
 
 function equals(a, b) {
+  const aIsPrimitive = isPrimitive(a);
+  const bIsPrimitive = isPrimitive(b);
   const typeOfA = typeof a;
   const typeOfB = typeof b;
 
-  if (isPrimitive(a) || isPrimitive(b)) {
+  if (aIsPrimitive || bIsPrimitive) {
     if (typeOfA === 'number' && typeOfB === 'number') {
       return isNaN(a) ? isNaN(b) : a === b;
     }
 
     return a === b;
+  }
+
+  if ((aIsPrimitive && !bIsPrimitive) || (!aIsPrimitive && bIsPrimitive)) {
+    return false;
   }
 
   return true;
