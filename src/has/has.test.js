695e150707526f68ecea36923ef4cee9865cbc6c
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-06 13:01:58 +0300

    add true and false tests to has module

--- src/has/has.test.js
+++ src/has/has.test.js
@@ -5,35 +5,11 @@ const hasHello = has('hello');
 const hasHelloPath = has(['hello', 'there']);
 
 test('has', (t) => {
-  t.equals(
-    typeof hasHello,
-    'function',
-    'returns curried function'
-  );
-
-  t.equals(
-    hasHello({ hello: 'world' }),
-    true,
-    'detects existing path'
-  );
-
-  t.equals(
-    hasHello({ there: 'you' }),
-    false,
-    'detects unexisting path'
-  );
-
-  t.equals(
-    hasHelloPath({ hello: { there: 'you' } }),
-    true,
-    'detects deeply nested existing path'
-  );
-
-  t.equals(
-    hasHelloPath({ hello: { you: 'there' } }),
-    false,
-    'detects deeply nested unexisting path'
-  );
+  t.equals(typeof hasHello, 'function', 'returns curried function');
+  t.true(hasHello({ hello: 'world' }), 'detects existing path');
+  t.false(hasHello({ there: 'you' }), 'detects unexisting path');
+  t.true(hasHelloPath({ hello: { there: 'you' } }), 'detects deeply nested existing path');
+  t.false(hasHelloPath({ hello: { you: 'there' } }), 'detects deeply nested unexisting path');
 
   t.end();
 });
