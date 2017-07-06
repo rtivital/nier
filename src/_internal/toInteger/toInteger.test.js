64a61beae29b7c6af51b34459211caacadba3533
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-06 12:48:59 +0300

    add internal test notation to toInteger function

--- src/_internal/toInteger/toInteger.test.js
+++ src/_internal/toInteger/toInteger.test.js
@@ -1,7 +1,7 @@
 const test = require('tape-catch');
 const toInteger = require('./toInteger');
 
-test('toInteger', (t) => {
+test('_internal/toInteger', (t) => {
   t.equals(toInteger(2.33), 2, 'converts float to integer');
   t.equals(toInteger('1'), 1, 'converts string to integer');
   t.equals(toInteger('nier'), 0, 'returns zero if unable to parse provided value');
