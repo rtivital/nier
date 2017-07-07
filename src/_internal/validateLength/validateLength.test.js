768d4244786ea2a10bc732039aae811904c8aa1c
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-07 13:35:17 +0300

    _internal/validateLength test refactoring

--- src/_internal/validateLength/validateLength.test.js
+++ src/_internal/validateLength/validateLength.test.js
@@ -2,20 +2,11 @@ const test = require('tape-catch');
 const validateLength = require('./validateLength');
 
 test('_internal/validateLength', (t) => {
-  t.true(
-    validateLength(10),
-    'returns true if length is valid number'
-  );
-
-  t.false(
-    validateLength(-1) || validateLength(2.2) || validateLength(Number.MAX_SAFE_INTEGER + 1),
-    'returns false if length is invalid number'
-  );
-
-  t.false(
-    validateLength(null) || validateLength('nier') || validateLength([]) || validateLength(Symbol('a')),
-    'returns false if length is invalid value type'
-  );
+  t.true(validateLength(10), 'returns true if length is valid number');
+  t.false(validateLength(-1), 'return false if length is less than zero');
+  t.false(validateLength(0.6), 'returns false if length is float');
+  t.false(validateLength(Number.MAX_SAFE_INTEGER + 1), 'returns false if length is greater than max safe integer');
+  t.false(validateLength('1'), 'returns false if length has a wrong type');
 
   t.end();
 });
