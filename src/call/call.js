0c113e2e5ab8f2192f82a3d341e3726210b3c2a1
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-07 12:42:48 +0300

    add call examples and docs

--- src/call/call.js
+++ src/call/call.js
@@ -3,11 +3,18 @@ const isFunction = require('../isFunction/isFunction');
 
 
 /**
- * Calls provided function with given arguments.
+ * Calls provided function (first argument) with given values (all other arguments).
+ *
+ * @since v1.0.0
  *
  * @param {Function} fn function to call
  * @param {...*} args list of arguments to call function with
  * @return {*} result of calling function with given list of arguments
+ *
+ * @example
+ * N.call(console.log, 'hello', 'nier'); // -> same as console.log('hello', 'nier')
+ * N.call(N.clamp(1, 10), 15); // -> same as N.clamp(1, 10, 15)
+ * N.call(N.has('nier'), { nier: 'hello' }); // -> same as N.has('nier', { nier: 'hello' })
  */
 function call(fn, ...args) {
   if (!isFunction(fn)) {
