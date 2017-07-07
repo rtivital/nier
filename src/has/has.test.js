cd7c850d262305ea95d1964904743da8ac57f137
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-07 13:23:01 +0300

    add new has module tests

--- src/has/has.test.js
+++ src/has/has.test.js
@@ -1,15 +1,19 @@
 const test = require('tape-catch');
 const has = require('./has');
 
-const hasHello = has('hello');
-const hasHelloPath = has(['hello', 'there']);
-
 test('has', (t) => {
-  t.equals(typeof hasHello, 'function', 'returns curried function');
-  t.true(hasHello({ hello: 'world' }), 'detects existing path');
-  t.false(hasHello({ there: 'you' }), 'detects unexisting path');
-  t.true(hasHelloPath({ hello: { there: 'you' } }), 'detects deeply nested existing path');
-  t.false(hasHelloPath({ hello: { you: 'there' } }), 'detects deeply nested unexisting path');
+  t.equals(typeof has('nier'), 'function', 'is curried');
+
+  t.false(has([], { a: 1 }), 'returns false if path is an empty array');
+  t.false(has('', { a: 1 }), 'returns false if path is an empty string');
+  t.false(has(null, { null: true }), 'returns false if path is null');
+  t.false(has([null, 'hello'], { null: { hello: 'nier' } }), 'returns false if array path contains invalid types');
+
+  t.true(has('nier', { nier: 1 }), 'returns true if valid string path was found in object');
+  t.false(has('nier', { hello: 1 }), 'returns false if valid string path was not found in object');
+
+  t.true(has(['hello', 'nier'], { hello: { nier: 1 } }), 'returns true if valid array path was found in object');
+  t.false(has(['hello', 'nier'], { hello: 1 }), 'return false if valid array path was not found in object');
 
   t.end();
 });
