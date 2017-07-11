f2f2e11fbbf988943a7bbd4f5de9d6660b67f713
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-11 19:38:53 +0300

    add normalize to format docs

--- scripts/docs/format.js
+++ scripts/docs/format.js
@@ -2,7 +2,7 @@ const N = require('../../src');
 
 const extractParams = N.pipe(
   N.filter(N.whereEq({ type: 'param' })),
-  N.reduce((acc, item) => N.set(item.name, item, acc), {})
+  N.normalize('name')
 );
 
 module.exports = function format(docItem) {
