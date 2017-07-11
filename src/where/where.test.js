1926c68a9011777aae6d11d6294b9bd1a8460603
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-11 12:08:23 +0300

    add where tests

---
+++ src/where/where.test.js
@@ -0,0 +1,42 @@
+const test = require('tape-catch');
+const where = require('./where');
+
+test('where', (t) => {
+  t.equals(
+    typeof where({}),
+    'function',
+    'is curried'
+  );
+
+  t.true(
+    where({})({ a: 1 }),
+    'returns true if predictates were not passed'
+  );
+
+  t.false(
+    where({ a: true })({ a: 1 }),
+    'returns false if any of predictates is not a function'
+  );
+
+  t.true(
+    where({ a: val => val === 1 })({ a: 1 }),
+    'returns true if passed predictate matches object'
+  );
+
+  t.true(
+    where({ a: val => val === 1, b: val => typeof val === 'string' })({ a: 1, b: 'nier' }),
+    'returns true if all provided predictates match provided object'
+  );
+
+  t.false(
+    where({ a: val => val === 1 })({ a: 2 }),
+    'returns false if passed predictate does not match provided object'
+  );
+
+  t.false(
+    where({ a: val => val === 1, b: val => typeof val === 'string' })({ a: 'hello', b: 'nier' }),
+    'returns false if any predictate does not match the object'
+  );
+
+  t.end();
+});
