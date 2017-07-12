b0ab1d38234b8ff69525dec950017c369d80aa5d
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 13:05:28 +0300

    add head to pipe

--- src/pipe/pipe.js
+++ src/pipe/pipe.js
@@ -1,12 +1,13 @@
 const reduce = require('../reduce/reduce');
 const slice = require('../slice/slice');
+const head = require('../head/head');
 
 module.exports = function pipe(...fns) {
   if (fns.length === 0) {
     throw new Error('pipe requires at least one function argument');
   }
 
-  const firstFunction = fns[0];
+  const firstFunction = head(fns);
   const tailFunctions = slice(1, fns.length, fns);
 
   return (...args) => reduce(
