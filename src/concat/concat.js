6680ca71a6c50e1983adc81529e8290a8fc9c0c5
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 13:16:00 +0300

    add new concat module

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -1,7 +1,22 @@
+const isArrayLike = require('../_internal/isArrayLike/isArrayLike');
+const cloneArrayLike = require('../_internal/cloneArrayLike/cloneArrayLike');
+const isString = require('../isString/isString');
 const curry = require('../curry/curry');
 
 function concat(a, b) {
-  return [...a, ...b];
+  if (!isArrayLike(a) || !isArrayLike(b)) {
+    throw new Error('N.concat received values that can not be concated');
+  }
+
+  const aClone = cloneArrayLike(a);
+  const bClone = cloneArrayLike(b);
+  const concated = aClone.concat(bClone);
+
+  if (isString(a) && isString(b)) {
+    return concated.join('');
+  }
+
+  return isString(a) && isString(b) ? concated.join('') : concated;
 }
 
 module.exports = curry(concat);
--- src/concat/concat.test.js
+++ src/concat/concat.test.js
@@ -2,9 +2,40 @@ const test = require('tape-catch');
 const concat = require('./concat');
 
 test('concat', (t) => {
-  t.equals(typeof concat([1, 2, 3]), 'function', 'is curried');
+  t.equals(
+    typeof concat([1, 2, 3]),
+    'function',
+    'is curried'
+  );
 
-  t.deepEquals(concat([1, 2], [3, 4]), [1, 2, 3, 4], 'concats two arrays');
+  t.throws(
+    () => concat(1, 2),
+    'throws if receives values that can not be concated'
+  );
+
+  t.deepEquals(
+    concat([1, 2], [3, 4]),
+    [1, 2, 3, 4],
+    'concats two arrays'
+  );
+
+  t.equals(
+    concat('hello, ', 'nier'),
+    'hello, nier',
+    'concats two strings'
+  );
+
+  t.deepEquals(
+    concat(['hello'], 'nier'),
+    ['hello', 'n', 'i', 'e', 'r'],
+    'concats string with array'
+  );
+
+  t.deepEquals(
+    concat({ 0: 'hello', length: 1 }, { 0: 'nier', length: 1 }),
+    ['hello', 'nier'],
+    'concats two array like values'
+  );
 
   t.end();
 });
