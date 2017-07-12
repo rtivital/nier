2e4146c2afff73a8c05f33f12551fd33f9c2182d
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 19:58:21 +0300

    remove old bundle size node script

--- scripts/bundleSize/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-const fs = require('fs-extra');
-
-fs.stat('./dist/index.js').then((file) => {
-  console.log(`${file.size / 1000}KB`);
-});
