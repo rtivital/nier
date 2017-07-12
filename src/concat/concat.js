efe3301c1e2dbf9cda3ad0b73cb341a50e3ab8bc
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 14:29:19 +0300

    remove duplicate logic from concat

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -3,6 +3,18 @@ const cloneArrayLike = require('../_internal/cloneArrayLike/cloneArrayLike');
 const isString = require('../isString/isString');
 const curry = require('../curry/curry');
 
+
+/**
+ * Concatenates two array or string values into one
+ *
+ * @since v1.0.0
+ *
+ * @param {Array|string} a fitst value to concat
+ * @param {Array|string} b second value to concat
+ * @return {Array|string} result of concatination
+ *
+ * @example
+ */
 function concat(a, b) {
   if (!isArrayLike(a) || !isArrayLike(b)) {
     throw new Error('N.concat received values that can not be concated');
@@ -11,10 +23,6 @@ function concat(a, b) {
   const aClone = cloneArrayLike(a);
   const bClone = cloneArrayLike(b);
   const concated = aClone.concat(bClone);
-
-  if (isString(a) && isString(b)) {
-    return concated.join('');
-  }
 
   return isString(a) && isString(b) ? concated.join('') : concated;
 }
