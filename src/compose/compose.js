2bd4a8027e024a98c68e9521e1de6a02db277a5f
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 12:34:40 +0300

    use pipe in compose instead

--- src/compose/compose.js
+++ src/compose/compose.js
@@ -1,15 +1,10 @@
 const reverse = require('../reverse/reverse');
+const pipe = require('../pipe/pipe');
 
 module.exports = function compose(...fns) {
   if (fns.length === 0) {
     throw new Error('compose requires at least one function argument');
   }
 
-  const lastFunction = fns[fns.length - 1];
-  const tailFunctions = reverse(fns.slice(0, fns.length - 1));
-
-  return (...args) => tailFunctions.reduce(
-    (acc, fn) => fn(acc),
-    lastFunction(...args)
-  );
+  return pipe(...reverse(fns));
 };
