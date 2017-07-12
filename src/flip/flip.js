beafdb229b7e60de7fd547da3dbcd6d5a3597b9a
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 14:47:16 +0300

    add reverse function to flip module

--- src/flip/flip.js
+++ src/flip/flip.js
@@ -1,8 +1,9 @@
 const isFunction = require('../isFunction/isFunction');
+const reverse = require('../reverse/reverse');
 
 module.exports = function flip(fn) {
   if (!isFunction(fn)) {
     throw new Error('flip received argument that is not function');
   }
-  return (...args) => fn(...args.reverse());
+  return (...args) => fn(...reverse(args));
 };
