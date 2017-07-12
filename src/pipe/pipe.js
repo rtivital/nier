3718c8405648f377afb5541333bf9d678a65a08b
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 12:36:58 +0300

    use N.reduce method in pipe

--- src/pipe/pipe.js
+++ src/pipe/pipe.js
@@ -1,3 +1,5 @@
+const reduce = require('../reduce/reduce');
+
 module.exports = function pipe(...fns) {
   if (fns.length === 0) {
     throw new Error('pipe requires at least one function argument');
@@ -6,8 +8,9 @@ module.exports = function pipe(...fns) {
   const firstFunction = fns[0];
   const tailFunctions = fns.slice(1);
 
-  return (...args) => tailFunctions.reduce(
+  return (...args) => reduce(
     (acc, fn) => fn(acc),
-    firstFunction(...args)
+    firstFunction(...args),
+    tailFunctions
   );
 };
