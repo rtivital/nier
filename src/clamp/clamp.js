5547568dc3372f65263a9ba54939e7973eb9524a
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-12 11:49:27 +0300

    add clamp docs

--- src/clamp/clamp.js
+++ src/clamp/clamp.js
@@ -1,5 +1,21 @@
 const curry = require('../curry/curry');
 
+
+/**
+ * Clamps number within provided min and max borders
+ *
+ * @since v1.0.0
+ *
+ * @param {number} min lower border
+ * @param {number} max upper border
+ * @param {number} value number to clamp
+ * @return {number} clamped number
+ *
+ * @example
+ * N.clamp(10, 20, 100); // -> 20
+ * N.clamp(-10, 20, -50); // -> -10
+ * N.clamp(1.5, 1.9, 1.937); // -> 1.9
+ */
 function clamp(min, max, value) {
   if (min > max) {
     throw new Error('min must not be greater than max in clamp');
