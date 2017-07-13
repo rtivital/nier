f8dd4e3eca4d702310ab6e597812990deeab253b
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 00:01:49 +0300

    add new findLastIndex tests

--- src/findLastIndex/findLastIndex.test.js
+++ src/findLastIndex/findLastIndex.test.js
@@ -1,31 +1,20 @@
 const test = require('tape-catch');
 const findLastIndex = require('./findLastIndex');
-
-const find2 = value => value === 2;
+const isCurried = require('../../testUtils/isCurried');
 
 test('findLastIndex', (t) => {
+  isCurried(t, findLastIndex(val => val > 2));
+
   t.equals(
-    typeof findLastIndex(find2),
-    'function',
-    'is curried'
+    findLastIndex(val => val > 2, [0, 1, 2, 3, 2]),
+    3,
+    'returns index of last matching element'
   );
 
   t.equals(
-    findLastIndex(find2, [0, 1, 2, 3, 4]),
-    2,
-    'returns index if it exists'
-  );
-
-  t.equals(
-    findLastIndex(find2, [2, 2, 2, 2, 2]),
-    4,
-    'returns last index that is found'
-  );
-
-  t.equals(
-    findLastIndex(find2, [0, 1, 3, 4]),
+    findLastIndex(val => val > 2, [0, 1, 0, 1]),
     -1,
-    'returns -1 if no value mached'
+    'returns -1 there is no matching elements'
   );
 
   t.end();
