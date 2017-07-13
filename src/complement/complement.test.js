18e066f41f43609e0e4bb05e2a59f7a2d293156b
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-13 13:01:14 +0300

    add curring test to complement

--- src/complement/complement.test.js
+++ src/complement/complement.test.js
@@ -1,16 +1,13 @@
 const test = require('tape-catch');
 const complement = require('./complement');
+const isCurried = require('../../testUtils/isCurried');
 
 test('complement', (t) => {
+  isCurried(t, complement());
+
   t.throws(
     () => complement(1),
     'throws if received argument is not function'
-  );
-
-  t.equals(
-    typeof complement(),
-    'function',
-    'is curried'
   );
 
   t.true(
