0f2053a9f8879e5ddad4b3a9908b9a128ce080c1
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 00:35:48 +0300

    add new noIndex test to every module

--- src/every/every.test.js
+++ src/every/every.test.js
@@ -1,9 +1,10 @@
 const test = require('tape-catch');
 const every = require('./every');
-const isCurried = require('../../testUtils/isCurried');
+const testUtils = require('../../testUtils');
 
 test('every', (t) => {
-  isCurried(t, every(() => true));
+  testUtils.isCurried(t, every(() => true));
+  testUtils.noIndex(t, { fn: every, data: [1, 2, 3], result: true });
 
   t.false(
     every(val => val === 2, [2, 2, 3]),
@@ -13,11 +14,6 @@ test('every', (t) => {
   t.true(
     every(val => val === 2, [2, 2, 2]),
     'returns true if all items passed the check'
-  );
-
-  t.true(
-    every((val, index, data) => index === undefined && data === undefined, [3, 2, 1]),
-    'does not provide index and data to callback'
   );
 
   t.end();
