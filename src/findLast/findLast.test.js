859a82babc4ca24e330cc4dd36e35605d3fc596d
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 00:41:57 +0300

    add noIndex test to findLast

--- src/findLast/findLast.test.js
+++ src/findLast/findLast.test.js
@@ -1,9 +1,10 @@
 const test = require('tape-catch');
 const findLast = require('./findLast');
-const isCurried = require('../../testUtils/isCurried');
+const testUtils = require('../../testUtils');
 
 test('findLast', (t) => {
-  isCurried(t, findLast(val => val > 2));
+  testUtils.isCurried(t, findLast(val => val > 2));
+  testUtils.noIndex(t, { fn: findLast, data: [0, 1, 2], result: 2 });
 
   t.equals(
     findLast(val => val > 2, [1, 2, 3, 4, 5, 2]),
@@ -15,12 +16,6 @@ test('findLast', (t) => {
     findLast(val => val > 2, [1, 1, 1]),
     undefined,
     'returns undefined if there is no matching elements in the array'
-  );
-
-  t.equals(
-    findLast((item, index, data) => index === undefined && data === undefined, [0, 1, 2]),
-    2,
-    'does not provide index and data to callback'
   );
 
   t.end();
