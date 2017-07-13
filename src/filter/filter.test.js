3097de12467ca8e767b369f161557cc84e21f06e
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 00:26:37 +0300

    add noIndex test util to filter function

--- src/filter/filter.test.js
+++ src/filter/filter.test.js
@@ -1,20 +1,16 @@
 const test = require('tape-catch');
 const filter = require('./filter');
 const isCurried = require('../../testUtils/isCurried');
+const noIndex = require('../../testUtils/noIndex');
 
 test('filter', (t) => {
   isCurried(t, filter(f => f));
+  noIndex(t, { fn: filter, data: [1, 2, 3, 4], result: [1, 2, 3, 4] });
 
   t.deepEquals(
     filter(f => !!f, [0, false, '', 1, true]),
     [1, true],
     'filters based on item'
-  );
-
-  t.deepEquals(
-    filter((item, index, data) => index === undefined && data === undefined, [1, 2, 3, 4]),
-    [1, 2, 3, 4],
-    'does not provide index and data to callback'
   );
 
   t.end();
