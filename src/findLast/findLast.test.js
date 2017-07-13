1200bf1639c88987067a7b9b599056a7cf725467
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 00:13:32 +0300

    add findLast tests for data and index providing

--- src/findLast/findLast.test.js
+++ src/findLast/findLast.test.js
@@ -6,7 +6,7 @@ test('findLast', (t) => {
   isCurried(t, findLast(val => val > 2));
 
   t.equals(
-    findLast(val => val > 2, [1, 2, 3, 4, 5]),
+    findLast(val => val > 2, [1, 2, 3, 4, 5, 2]),
     5,
     'finds last matching element'
   );
@@ -15,6 +15,12 @@ test('findLast', (t) => {
     findLast(val => val > 2, [1, 1, 1]),
     undefined,
     'returns undefined if there is no matching elements in the array'
+  );
+
+  t.equals(
+    findLast((item, index, data) => index === undefined && data === undefined, [0, 1, 2]),
+    2,
+    'does not provide index and data to callback'
   );
 
   t.end();
