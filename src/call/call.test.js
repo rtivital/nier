edbbdf838bae572a18d57426098c5013ed681878
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-13 12:58:17 +0300

    add is curried test util to call module

--- src/call/call.test.js
+++ src/call/call.test.js
@@ -1,9 +1,11 @@
 const test = require('tape-catch');
 const call = require('./call');
+const isCurried = require('../../testUtils/isCurried');
 
 test('call', (t) => {
+  isCurried(t, call(f => f));
+
   t.throws(() => call(1, 'hello'), 'throws error if received non-function first argument');
-  t.equals(typeof call(f => f), 'function', 'is curried');
   t.equals(call(f => f, 1), 1, 'provides single argument to callback function');
   t.equals(call((a, b) => a + b)(2, 45), 47, 'provides multiple arguments to callback function');
 
