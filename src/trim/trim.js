2f01e70ce96618106a6b77aea6f483fb8cad9c0a
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 11:55:15 +0300

    remove uniq and trim modules for now

--- src/trim/trim.js
+++
@@ -1,9 +0,0 @@
-const isString = require('../isString/isString');
-
-module.exports = function trim(str) {
-  if (!isString(str)) {
-    throw new Error('trim received argument that is not string');
-  }
-
-  return str.trim();
-};
--- src/uniq/uniq.js
+++
@@ -1,3 +0,0 @@
-module.exports = function uniq(array) {
-  return [...(new Set(array))];
-};
