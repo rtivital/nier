821cd676aa3dda0f2b0550f13c1a8af48211fbd3
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 10:05:40 +0300

    add new map tests

--- src/map/map.test.js
+++ src/map/map.test.js
@@ -1,27 +1,15 @@
 const test = require('tape-catch');
 const map = require('./map');
-
-const multiplyBy2 = value => value * 2;
-const multiplyByIndex = (value, index) => value * index;
-const mapMultiply = map(multiplyBy2);
+const testUtils = require('../../testUtils');
 
 test('map', (t) => {
-  t.deepEquals(
-    map(multiplyBy2, [1, 2, 3]),
-    [1, 2, 3].map(multiplyBy2),
-    'works as regular map function'
-  );
+  testUtils.isCurried(t, map(val => val * 2));
+  testUtils.noIndex(t, { fn: map, data: [1, 2, 3], result: [true, true, true] });
 
   t.deepEquals(
-    mapMultiply([1, 2, 3]),
-    [1, 2, 3].map(multiplyBy2),
-    'supports curring'
-  );
-
-  t.deepEquals(
-    map(multiplyByIndex, true, [1, 2, 3]),
-    [1, 2, 3].map(multiplyByIndex),
-    'passes index down when it isn required'
+    map(val => val * 2, [1, 2, 3]),
+    [2, 4, 6],
+    'maps over array'
   );
 
   t.end();
