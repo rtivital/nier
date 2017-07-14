80d4cb7dd4ed75edb3f857e67fa89b59e55f9f54
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 10:05:33 +0300

    add new map module with curry

--- src/map/map.js
+++ src/map/map.js
@@ -1,13 +1,13 @@
-const iterator = require('../iterator/iterator');
+const curry = require('../curry/curry');
 
-function map(mapFn, data) {
+function map(callback, data) {
   const result = [];
 
-  for (let i = 0, length = data.length; i < length; i += 1) {
-    result.push(mapFn(data[i], i));
+  for (let i = 0, l = data.length; i < l; i += 1) {
+    result.push(callback(data[i]));
   }
 
   return result;
 }
 
-module.exports = iterator(map, 'map');
+module.exports = curry(map);
