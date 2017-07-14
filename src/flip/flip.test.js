daa2248e42463c97a8e59e558488e6be02624a3b
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-14 11:35:04 +0300

    add new flip module tests

--- src/flip/flip.test.js
+++ src/flip/flip.test.js
@@ -1,38 +1,36 @@
 const test = require('tape-catch');
 const flip = require('./flip');
+const testUtils = require('../../testUtils');
 
-const getZero = () => 0;
-const greet = name => `Hello, ${name}`;
-const concat = (str1, str2) => `${str1} ${str2}`;
-const concat3 = (str1, str2, str3) => `${str1} ${str2} ${str3}`;
+test.only('flip', (t) => {
+  testUtils.isCurried(t, flip());
 
-test('flip', (t) => {
   t.throws(
     () => flip(1),
     'trows error if provided argument is not a function'
   );
 
   t.equals(
-    flip(getZero)(),
-    getZero(),
+    flip(() => 0)(),
+    0,
     'does not change the behavior of function with no arguments'
   );
 
   t.equals(
-    flip(greet)('John'),
-    greet('John'),
+    flip(name => `Hello, ${name}!`)('nier'),
+    'Hello, nier!',
     'does not change the behavior of function with single argument'
   );
 
   t.equals(
-    flip(concat)('world', 'hello'),
-    concat('hello', 'world'),
+    flip((str1, str2) => `${str1} ${str2}`)('nier!', 'Hello,'),
+    'Hello, nier!',
     'flips function with two arguments'
   );
 
   t.equals(
-    flip(concat3)('!', 'world', 'hello'),
-    concat3('hello', 'world', '!'),
+    flip((str1, str2, str3) => `${str1} ${str2} ${str3}`)('nier!', 'there', 'Hello,'),
+    'Hello, there nier!',
     'flips function with three arguments'
   );
 
