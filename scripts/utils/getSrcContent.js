ed99eb75a56cb67c10644f2c4c39f57adf5c4c95
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-15 22:39:25 +0300

    add src content pasring util

---
+++ scripts/utils/getSrcContent.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const fs = require('fs-extra');
+const N = require('../../src');
+
+const INTERNAL_RESOURCES = [
+  '_internal',
+  'index.js',
+];
+
+const removeInternalResources = N.without(INTERNAL_RESOURCES);
+
+module.exports = function getSrcContent() {
+  return fs.readdir('./src').then(removeInternalResources);
+};
