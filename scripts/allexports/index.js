e5e8f9da6eb999f8da75537c5149b17c44b92da2
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-15 22:44:28 +0300

    add allexports script

---
+++ scripts/allexports/index.js
@@ -0,0 +1,12 @@
+const getSrcContent = require('../utils/getSrcContent');
+const N = require('../../src');
+
+const exportedModules = N.keys(N);
+
+getSrcContent().then((modules) => {
+  N.map((module) => {
+    if (!N.includes(module, exportedModules)) {
+      console.log(`Missing module export ${module}`);
+    }
+  }, modules);
+});
