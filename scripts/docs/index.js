cc4a65a665c5ba56213ddc323094a915c3204d87
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-15 22:40:44 +0300

    remove eslint import/no-extraneous-dependencies rule from base config

--- scripts/docs/index.js
+++ scripts/docs/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 const fs = require('fs-extra');
 const dox = require('dox');
 const format = require('./format');
--- scripts/utils/getSrcContent.js
+++ scripts/utils/getSrcContent.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 const fs = require('fs-extra');
 const N = require('../../src');
 
