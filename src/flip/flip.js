91dfaf96539d1900a3869464c58fddc224e71f39
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-16 23:06:10 +0300

    add arity saving to flip function

--- src/flip/flip.js
+++ src/flip/flip.js
@@ -1,3 +1,4 @@
+const arity = require('../_internal/arity/arity');
 const isFunction = require('../isFunction/isFunction');
 const reverse = require('../reverse/reverse');
 const curry = require('../curry/curry');
@@ -7,7 +8,7 @@ function flip(fn) {
     throw new Error('N.flip received argument that is not function');
   }
 
-  return (...args) => fn(...reverse(args));
+  return arity(fn.length, (...args) => fn(...reverse(args)));
 }
 
 module.exports = curry(flip);
