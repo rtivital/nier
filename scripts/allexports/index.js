9c80a25479bfd6b4fe78cc90600cd6fa668829ca
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-16 15:51:27 +0300

    move allexports title to variable

--- scripts/allexports/index.js
+++ scripts/allexports/index.js
@@ -1,23 +1,25 @@
 /* eslint-disable no-console */
+
 const chalk = require('chalk');
 const getSrcContent = require('../utils/getSrcContent');
 const N = require('../../src');
 
 const exportedModules = N.keys(N);
+const title = chalk.blue.bold('allexports:');
 
 getSrcContent().then((modules) => {
   const missingModules = N.filter(module => !N.includes(module, exportedModules), modules);
 
   if (missingModules.length === 0) {
     console.log(
-      `${chalk.blue.bold('allexports:')} No missing modules`
+      `${title} No missing modules`
     );
 
     process.exit(0);
   }
 
   const messages = N.map(
-    module => `${chalk.blue.bold('allexports:')} Missing module: ${chalk.red.bold(module)}`,
+    module => `${title} Missing module: ${chalk.red.bold(module)}`,
     missingModules
   );
 
