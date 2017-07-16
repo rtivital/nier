ce6e546a4305a9ac2198873d098f2d34bb94f517
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-16 22:40:09 +0300

    enable custom messages in isCurried test util

--- testUtils/isCurried.js
+++ testUtils/isCurried.js
@@ -1,3 +1,3 @@
-module.exports = function isCurried(t, value) {
-  t.equals(typeof value, 'function', 'is curried');
+module.exports = function isCurried(t, value, message = 'is curried') {
+  t.equals(typeof value, 'function', message);
 };
