e3e70dbf73087e5b5d16b54999ff42ae223ada39
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 13:07:44 +0300

    add some docs

--- src/some/some.js
+++ src/some/some.js
@@ -1,5 +1,22 @@
 const curry = require('../curry/curry');
 
+/**
+ * Returns true if any element match the predicate, false otherwise.
+ *
+ * @since v1.0.0
+ *
+ * @param {Function} callback predicate, will be called with each item
+ * @param {Array} list
+ * @return {boolean}
+ *
+ * @see some, every
+ * @alias any
+ *
+ * @example
+ * N.some((number) => number > 2, [1, 2, 3]); // -> true
+ * N.some((number) => number < 2, [2, 3, 4]); // -> false
+ * N.some(N.path('nier'))([{ nier: 'hello' }, { hello: true }]); // -> true
+ */
 function some(callback, data) {
   for (let i = 0, length = data.length; i < length; i += 1) {
     if (callback(data[i])) {
