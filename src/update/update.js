7eb693b8e64b5e2110d1c8d9d0ced69f3ce884cb
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 17:49:51 +0300

    add update docs

--- src/update/update.js
+++ src/update/update.js
@@ -1,6 +1,23 @@
 const curry = require('../curry/curry');
 const has = require('../has/has');
 
+/**
+ * Creates new copy of the array with the element at index replaced with provided value.
+ * If array does not have value at provided index, it won't be set and the original array will be returned.
+ * Note that update will only create shallow copy of the array.
+ *
+ * @since v1.0.0
+ *
+ * @param {number} index position to update
+ * @param {*} value value to set at position
+ * @param {Array} array
+ * @return {Array} shallow array copy
+ *
+ * @example
+ * N.update(0, 'hello', ['hi', 'nier']); // -> ['hello', 'nier']
+ * N.update(5, 'hello', ['hi', 'nier']); // -> ['hi', 'nier']
+ * N.update(-1, 'nier', ['hi', 'there', 'you']); // -> ['hi', 'there', 'nier']
+ */
 function update(index, value, array) {
   const { length } = array;
   const indexToUpdate = index < 0 ? length + index : index;
