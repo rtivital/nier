be837a9bc7b65919c6a339f3a9762733845be966
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 13:21:47 +0300

    add toLowerCase docs

--- src/toLowerCase/toLowerCase.js
+++ src/toLowerCase/toLowerCase.js
@@ -1,6 +1,18 @@
 const isString = require('../isString/isString');
 const curry = require('../curry/curry');
 
+
+/**
+ * Convers all string characters to lower case.
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ * N.toLowerCase('Hello!'); // -> 'hello!'
+ * N.toLowerCase('HELLO!'); // -> 'hello!'
+ * N.toLowerCase('hello!'); // -> 'hello!'
+ */
 function toLowerCase(str) {
   if (!isString(str)) {
     throw new Error('N.toLowerCase received argument that is not string');
