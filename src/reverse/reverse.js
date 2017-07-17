ef30dde82993f84f81d083301c5dff2563bbd856
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 13:05:38 +0300

    add reverse docs

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -2,6 +2,19 @@ const isArrayLike = require('../_internal/isArrayLike/isArrayLike');
 const isString = require('../isString/isString');
 const curry = require('../curry/curry');
 
+
+/**
+ * Returns new collection with reversed order.
+ *
+ * @since v1.0.0
+ *
+ * @param {Array|string} collection data to reverse
+ * @return {Array|string}
+ *
+ * @example
+ * N.reverse([1, 2, 3]); // -> [3, 2, 1]
+ * N.reverse('nier'); // -> 'rein'
+ */
 function reverse(collection) {
   if (!isArrayLike(collection)) {
     throw new Error('N.reverse is not able to work with data that is not array or array like value');
