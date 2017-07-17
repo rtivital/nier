5373fb1410749e65ba6beaaf0228ad77bab0b7cb
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 13:02:27 +0300

    add min docs

--- src/min/min.js
+++ src/min/min.js
@@ -1,5 +1,20 @@
 const curry = require('../curry/curry');
 
+/**
+ * Returns the min of its two arguments.
+ *
+ * @since v1.0.0
+ *
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ *
+ * @see max
+ *
+ * @example
+ * N.min(1, 2); // -> 1
+ * N.min('a', 'b'); // -> 'a'
+ */
 function min(a, b) {
   return a > b ? b : a;
 }
