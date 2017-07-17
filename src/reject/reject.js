77ccc3556c8f86633232c3c703e6f7bc50d8ce99
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 12:43:18 +0300

    add reject docs

--- src/reject/reject.js
+++ src/reject/reject.js
@@ -1,5 +1,23 @@
 const curry = require('../curry/curry');
 
+/**
+ * Removes item from list if it matches the predicate.
+ *
+ * @since v1.0.0
+ *
+ * @param {Function} callback predicate
+ * @param {Array} list array to filter
+ * @return {Array} filtered list
+ *
+ * @see filter
+ *
+ * @example
+ * N.reject((item) => item > 2)([1, 2, 3, 4]); // -> [1, 2]
+ * N.reject(
+ *   N.whereEq({ id: null }),
+ *   [{ id: null, name: 1 }, { id: 'nier', name: 2 }, { id: null, name: 3 }]
+ * ); // -> [{ id: 'nier', name: 2 }]
+ */
 function reject(callback, data) {
   const result = [];
 
