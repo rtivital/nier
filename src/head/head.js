a1dac3bc80e938f3bf22d76c92fa1184a0244e75
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 12:47:44 +0300

    add head docs

--- src/head/head.js
+++ src/head/head.js
@@ -1,6 +1,20 @@
 const isArrayLike = require('../_internal/isArrayLike/isArrayLike');
 const curry = require('../curry/curry');
 
+
+/**
+ * Returns first item of array like collection
+ *
+ * @since v1.0.0
+ *
+ * @param {Array} collection
+ * @return {any} first item of collection
+ *
+ * @example
+ * N.head([1, 2, 3]); // -> 1
+ * N.head('nier'); // -> 'n'
+ * N.head({ 0: 'first', 1: 'second', length: 2 }); // -> first
+ */
 function head(collection) {
   if (isArrayLike(collection)) {
     return collection[0];
