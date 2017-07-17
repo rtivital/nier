bb8d9b42476d4e806dd04b1ec33712f142a4db70
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 12:09:50 +0300

    add curry docs

--- src/curry/curry.js
+++ src/curry/curry.js
@@ -1,5 +1,21 @@
 const curryN = require('../curryN/curryN');
 
+
+/**
+ * Returns curried equivalent of provided function.
+ *
+ * @since v1.0.0
+ *
+ * @param {Function} fn function to curry
+ * @return {Function} curried function
+ *
+ * @example
+ * const sum = N.curry((a, b, c) => a + b + c);
+ * sum(1, 2, 3); // 6
+ * sum(1, 2)(3); // 6
+ * sum(1)(2)(3); // 6
+ * sum(1)()(2)()(3); //6
+ */
 function curry(fn) {
   return curryN(fn.length, fn);
 }
