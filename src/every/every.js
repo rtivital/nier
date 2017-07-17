bc6dcaed7b4758462eac0098c576fed15b37f2e3
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 12:23:05 +0300

    add every docs

--- src/every/every.js
+++ src/every/every.js
@@ -1,8 +1,25 @@
 const curry = require('../curry/curry');
 
-function every(callback, data) {
-  for (let i = 0, l = data.length; i < l; i += 1) {
-    if (!callback(data[i])) {
+/**
+ * Returns true if all elements match the predicate, false otherwise
+ *
+ * @since v1.0.0
+ *
+ * @param {Function} callback predicate, will be called with each item
+ * @param {Array} list
+ * @return {boolean}
+ *
+ * @see some, none
+ * @alias all
+ *
+ * @example
+ * N.every((number) => number > 2, [1, 2, 3]); // -> false
+ * N.every((number) => number > 2, [2, 3, 4]); // -> true
+ * N.every(N.path('nier'))([{ nier: 'hello' }, { hello: true }]); // -> false
+ */
+function every(callback, list) {
+  for (let i = 0, l = list.length; i < l; i += 1) {
+    if (!callback(list[i])) {
       return false;
     }
   }
