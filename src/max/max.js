0b00f0e3a5bc4d3d990b84bde9d5345bc3f644bd
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-17 13:01:56 +0300

    add max docs

--- src/max/max.js
+++ src/max/max.js
@@ -1,5 +1,21 @@
 const curry = require('../curry/curry');
 
+
+/**
+ * Returns the max of its two arguments.
+ *
+ * @since v1.0.0
+ *
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ *
+ * @see min
+ *
+ * @example
+ * N.max(1, 2); // -> 2
+ * N.max('a', 'b'); // -> 'b'
+ */
 function max(a, b) {
   return a > b ? a : b;
 }
