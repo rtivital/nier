3e98d87d83ba8148f28a467012d0dd5722703ed9
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-18 12:46:25 +0300

    add new partialRight with curry and partialRight docs

--- src/partialRight/partialRight.js
+++ src/partialRight/partialRight.js
@@ -1,9 +1,27 @@
-const isFunction = require('../isFunction/isFunction');
+const curry = require('../curry/curry');
 
-module.exports = function partial(fn, ...predefined) {
-  if (!isFunction(fn)) {
-    throw new Error('partialRight received first argument that is not a function');
+
+/**
+ * Binds function to specified argument list.
+ *
+ * @since v1.0.0
+ *
+ * @param {Function} fn function that should be binded
+ * @param {Array} predefined list of arguments that function should be binded to
+ * @return {Function} new function, binded to arguments
+ *
+ * @see partial
+ *
+ * @example
+ * N.partialRight((a, b) => a / b, [3])(6); // -> 2
+ * N.partial(greet, name) => `${greet} ${name}`, ['nier'])('hello'); // -> 'hello nier'
+ */
+function partial(fn, predefined) {
+  if (typeof fn !== 'function') {
+    throw new Error('N.partialRight did not receive function');
   }
 
-  return (...args) => fn(...args, ...predefined);
-};
+  return (...args) => fn(...predefined, ...args);
+}
+
+module.exports = curry(partial);
