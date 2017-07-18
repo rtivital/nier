0f937093da720b04f9b6bd4e7fa2e611050d4df1
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-18 17:00:23 +0300

    add new getMissingModules function with flip

--- scripts/allexports/index.js
+++ scripts/allexports/index.js
@@ -6,13 +6,14 @@ const N = require('../../src');
 
 const exportedModules = N.keys(N);
 const title = chalk.blue.bold('allexports:');
+const getMissingModules = N.reject(N.flip(N.includes)(exportedModules));
 const getMissingMessages = N.pipe(
   N.map(module => `${title} Missing module: ${chalk.red.bold(module)}`),
   N.join('\n')
 );
 
 getSrcContent().then((modules) => {
-  const missingModules = N.filter(module => !N.includes(module, exportedModules), modules);
+  const missingModules = getMissingModules(modules);
 
   if (missingModules.length === 0) {
     console.log(
