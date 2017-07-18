ddb6f4e63523de0ba1c65af2edd1699706183d02
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-18 20:01:31 +0300

    setup docs formatter

--- scripts/docs/format.js
+++ scripts/docs/format.js
@@ -1,10 +1,39 @@
 const N = require('../../src');
 
+const extractSince = N.pipe(
+  N.path('tags'),
+  N.find(N.whereEq({ type: 'since' })),
+  N.path('string')
+);
+
+const extractCategory = N.pipe(
+  N.path('tags'),
+  N.find(N.whereEq({ type: 'category' })),
+  N.path('string')
+);
+
+const extractExamples = N.pipe(
+  N.path('tags'),
+  N.find(N.whereEq({ type: 'example' })),
+  N.path('string')
+);
+
+const extractDescription = N.path(['description', 'full']);
+const extractName = N.path(['ctx', 'name']);
+
 const extractParams = N.pipe(
+  N.path('tags'),
   N.filter(N.whereEq({ type: 'param' })),
-  N.normalize('name')
+  N.map(item => ({ name: item.name, description: item.description, type: item.typesDescription }))
 );
 
 module.exports = function format(docItem) {
-  return extractParams(docItem.tags);
+  return {
+    name: extractName(docItem),
+    since: extractSince(docItem),
+    category: extractCategory(docItem),
+    examples: extractExamples(docItem),
+    description: extractDescription(docItem),
+    params: extractParams(docItem),
+  };
 };
--- scripts/docs/index.js
+++ scripts/docs/index.js
@@ -2,9 +2,14 @@ const fs = require('fs-extra');
 const dox = require('dox');
 const getSrcContent = require('../utils/getSrcContent');
 const N = require('../../src');
+const format = require('./format');
 
 const readSrcFiles = N.map(file => fs.readFile(`./src/${file}/${file}.js`));
-const parseComments = N.map(file => dox.parseComments(file.toString('utf8')));
+const parseComments = N.pipe(
+  N.map(file => dox.parseComments(file.toString('utf8'))),
+  N.unnest,
+  N.map(format)
+);
 
 getSrcContent()
   .then(files => Promise.all(readSrcFiles(files)))
