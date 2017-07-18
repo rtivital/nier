344e78cd2bccdc7fcd09926b517b1e0811cf1e55
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-18 16:56:15 +0300

    fix flip behaviour with curryN

--- src/flip/flip.js
+++ src/flip/flip.js
@@ -1,6 +1,7 @@
-const arity = require('../_internal/arity/arity');
+// const arity = require('../_internal/arity/arity');
 const reverse = require('../reverse/reverse');
 const curry = require('../curry/curry');
+const curryN = require('../curryN/curryN');
 
 
 /**
@@ -21,7 +22,7 @@ function flip(fn) {
     throw new Error('N.flip received argument that is not function');
   }
 
-  return arity(fn.length, (...args) => fn(...reverse(args)));
+  return curryN(fn.length, (...args) => fn(...reverse(args)));
 }
 
 module.exports = curry(flip);
