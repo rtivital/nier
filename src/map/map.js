4ab6e411a70c7528d5a3c74549664c37781484a2
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-18 11:39:20 +0300

    add map docs

--- src/map/map.js
+++ src/map/map.js
@@ -1,5 +1,19 @@
 const curry = require('../curry/curry');
 
+
+/**
+ * Creates a new array with the results of calling a provided function on every element in the calling array.
+ *
+ * @since v1.0.0
+ *
+ * @param {fn} callback function that will be called on each element
+ * @param {Array} data
+ * @return {Array}
+ *
+ * @example
+ * N.map((val) => val * 2, [1, 2, 3]); // -> [2, 4, 6]
+ * N.map((name) => `Hello, ${name}!`, ['nier', 'you']); // -> ['Hello, nier!', 'Hello, you!']
+ */
 function map(callback, data) {
   const result = [];
 
