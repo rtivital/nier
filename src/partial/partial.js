c4046df4b4fd425e490504fd01e7a530b4388e66
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-18 12:38:22 +0300

    add partial docs

--- src/partial/partial.js
+++ src/partial/partial.js
@@ -1,6 +1,20 @@
 const curry = require('../curry/curry');
 
 
+/**
+ * Binds function to provided argument list.
+ *
+ * @since v1.0.0
+ *
+ * @param {Function} fn function that should be binded
+ * @param {Array} predefined list of arguments that function should be binded to
+ * @return {Function} new function, binded to arguments
+ *
+ * @see partialRight
+ *
+ * @example
+ * N.partial((a, b) => a + b, [1])(2); // -> 3
+ */
 function partial(fn, predefined) {
   if (typeof fn !== 'function') {
     throw new Error('N.partial did not receive function');
