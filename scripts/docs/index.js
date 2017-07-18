021e5b87b6fdbc1f40ea6466371c7cd7cd2813df
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-18 19:34:56 +0300

    add docs generator

--- scripts/docs/index.js
+++ scripts/docs/index.js
@@ -1,10 +1,21 @@
 const fs = require('fs-extra');
 const dox = require('dox');
-const format = require('./format');
+const getSrcContent = require('../utils/getSrcContent');
+const N = require('../../src');
 
-fs
-  .readFile('./src/append/append.js')
-  .then((file) => {
-    const result = dox.parseComments(file.toString('utf8'));
-    console.log(JSON.stringify(format(result[0])));
+const readSrcFiles = N.map(file => fs.readFile(`./src/${file}/${file}.js`));
+const parseComments = N.map(file => dox.parseComments(file.toString('utf8')));
+
+getSrcContent()
+  .then(files => Promise.all(readSrcFiles(files)))
+  .then(files => fs.writeJson('./docs/data.json', parseComments(files)))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
   });
+// fs
+//   .readFile('./src/append/append.js')
+//   .then((file) => {
+//     const result = dox.parseComments(file.toString('utf8'));
+//     console.log(JSON.stringify(format(result[0])));
+//   });
