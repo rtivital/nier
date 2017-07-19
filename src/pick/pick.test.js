f54d808a91f14113b0bc8c9b8167ba6d1d3db7ff
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-19 11:58:10 +0300

    add pick tests

---
+++ src/pick/pick.test.js
@@ -0,0 +1,27 @@
+const test = require('tape-catch');
+const testUtils = require('../../testUtils');
+const pick = require('./pick');
+
+test('pick', (t) => {
+  testUtils.isCurried(t, pick([]));
+
+  t.deepEquals(
+    pick(['a', 'b'], { a: 1, b: 2, c: 3 }),
+    { a: 1, b: 2 },
+    'picks specified properties'
+  );
+
+  t.deepEquals(
+    pick(['a', 'b', 'd'], { a: 1, b: 2, c: 3 }),
+    { a: 1, b: 2 },
+    'ignores extraneous keys'
+  );
+
+  t.deepEquals(
+    pick([], { a: 1, b: 2, c: 3 }),
+    {},
+    'returns empty object if keys are not specified'
+  );
+
+  t.end();
+});
