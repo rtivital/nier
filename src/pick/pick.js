12177eb6c4d7ae4331a9d54b1e8edcf7a93f590f
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-19 11:54:13 +0300

    add pick docs

--- src/pick/pick.js
+++ src/pick/pick.js
@@ -2,6 +2,21 @@ const curry = require('../curry/curry');
 const reduce = require('../reduce/reduce');
 const has = require('../has/has');
 
+/**
+ * Takes a list of keys and an object. Returns shallow copy of object that will contain only specified keys.
+ * If key is missing in object it will be ignored.
+ *
+ * @since v1.0.0
+ *
+ * @param {Array} keys list of keys that should be included in new object
+ * @param {Object} object
+ * @return {Object}
+ *
+ * @example
+ * N.pick(['a', 'b'], { a: 1, b: 2, c: 3 }); // -> { a: 1, b: 2 }
+ * N.pick(['a', 'b', 'd'], { a: 1, b: 2, c: 3 }); // -> { a: 1, b: 2 }
+ * N.pick([], { a: 1, b: 2, c: 3 }); // -> {}
+ */
 function pick(keys, object) {
   return reduce((acc, key) => {
     if (has(key, object)) {
