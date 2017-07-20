534938cd66a8e3954d54dcf14fc7ed93e56f4f63
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 12:28:37 +0300

    add map withIndex test utils

--- src/map/map.test.js
+++ src/map/map.test.js
@@ -1,22 +1,16 @@
 const test = require('tape-catch');
 const map = require('./map');
 const testUtils = require('../../testUtils');
-const withIndex = require('../withIndex/withIndex');
 
 test.only('map', (t) => {
   testUtils.isCurried(t, map(val => val * 2));
   testUtils.noIndex(t, { fn: map, data: [1, 2, 3], result: [true, true, true] });
+  testUtils.withIndex(t, { fn: map, data: [1, 2], indexResult: [0, 1], dataResult: [[1, 2], [1, 2]] });
 
   t.deepEquals(
     map(val => val * 2, [1, 2, 3]),
     [2, 4, 6],
     'maps over array'
-  );
-
-  t.deepEquals(
-    withIndex(map)((val, index) => index * 2, [1, 2, 3]),
-    [0, 2, 4],
-    'works with withIndex module'
   );
 
   t.end();
