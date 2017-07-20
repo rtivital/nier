af063c7dc6d9106ac278ae9411a492ae6a17cdb6
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 19:36:27 +0300

    add fill docs

--- src/fill/fill.js
+++ src/fill/fill.js
@@ -1,6 +1,20 @@
 const curry = require('../curry/curry');
 
 
+/**
+ * Creates new array with provided length and fills it with provided value
+ *
+ * @since v1.0.0
+ * @category Array
+ *
+ * @param {number} length length of the array that should be filled
+ * @param {*} element element that should be used as each item in the array
+ * @return {Array}
+ *
+ * @example
+ * N.fill(3, 'hello'); // -> ['hello', 'hello', 'hello']
+ * N.fill(2, { a: 1 }); // -> [{ a: 1 }, { a: 1 }]
+ */
 function fill(length, element) {
   const array = [];
 
