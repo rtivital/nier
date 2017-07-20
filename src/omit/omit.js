293d4a049ca268bac89202d6209ba19a7e509a79
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 11:40:59 +0300

    add omit docs

--- src/omit/omit.js
+++ src/omit/omit.js
@@ -3,6 +3,23 @@ const reduce = require('../reduce/reduce');
 const keys = require('../keys/keys');
 const includes = require('../includes/includes');
 
+/**
+ * Takes a list of keys and an object. Returns shallow copy of object that will not contain specified keys.
+ * If key is missing in object it will be ignored.
+ *
+ * @since v1.0.0
+ *
+ * @param {Array} keys list of keys that should not be included in new object
+ * @param {Object} object
+ * @return {Object}
+ *
+ * @see pick
+ *
+ * @example
+ * N.omit(['a', 'b'], { a: 1, b: 2, c: 3 }); // -> { c: 3 }
+ * N.omit(['a', 'b', 'd'], { a: 1, b: 2, c: 3 }); // -> { c: 3 }
+ * N.omit([], { a: 1, b: 2, c: 3 }); // -> { a: 1, b: 2, c: 3 }
+ */
 function omit(remove, object) {
   return reduce((acc, key) => {
     if (!includes(key, remove)) {
