5bd4513ccf9e75fedf62701020e19cde50f23d19
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 12:02:05 +0300

    add withIndex map tests

--- src/withIndex/withIndex.test.js
+++ src/withIndex/withIndex.test.js
@@ -1,9 +1,23 @@
 const test = require('tape-catch');
 const testUtils = require('../../testUtils');
 const withIndex = require('./withIndex');
+const map = require('../map/map');
 
 test('withIndex', (t) => {
   testUtils.isCurried(t, withIndex);
+  testUtils.isCurried(t, withIndex(map)(val => val * 2), 'produces curriediterator function');
+
+  t.deepEquals(
+    withIndex(map)((_, index) => index + 1, [10, 20, 30]),
+    [1, 2, 3],
+    'provides index to iterator function'
+  );
+
+  t.deepEquals(
+    withIndex(map)((_, __, array) => array, [10, 20, 30]),
+    [[10, 20, 30], [10, 20, 30], [10, 20, 30]],
+    'provides array to iterator function'
+  );
 
   t.end();
 });
