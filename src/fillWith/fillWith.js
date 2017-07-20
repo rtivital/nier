f3bf3b0f52ae5bfebac0900cea084a44e745d7fe
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 19:41:30 +0300

    add fillWithModule

---
+++ src/fillWith/fillWith.js
@@ -0,0 +1,31 @@
+const curry = require('../curry/curry');
+
+
+/**
+ * Creates new array with provided length and fills it with the rusult of calling
+ * provided callback function with item index.
+ *
+ * @since v1.0.0
+ * @category Array
+ *
+ * @param {number} length length of the array that should be filled
+ * @param {Function} callback function that will produce item which will be added to array
+ * @return {Array}
+ *
+ * @see fill
+ *
+ * @example
+ * N.fillWith(3, (index) => `hello-${index}`); // -> ['hello-0', 'hello-1', 'hello-2']
+ * N.fillWith(2, (index) => ({ a: 'hello', b: index })); // -> [{ a: 'hello', b: 0 }, { a: 'hello', b: 1 }]
+ */
+function fill(length, element) {
+  const array = [];
+
+  for (let i = 0; i < length; i += 1) {
+    array.push(element);
+  }
+
+  return array;
+}
+
+module.exports = curry(fill);
