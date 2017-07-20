b6b6de1b178b4b6e14e94dcf37533cb62406b184
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 16:10:49 +0300

    add assign signature module to createReversedIterator function

--- src/_internal/createReversedIterator/createReversedIterator.js
+++ src/_internal/createReversedIterator/createReversedIterator.js
@@ -1,15 +1,7 @@
 const curry = require('../../curry/curry');
-const SIGNATURES = require('../signatures');
+const assignSignature = require('../assignSignature/assignSignature');
 
 module.exports = function createReversedIterator(iterator) {
   const reversedIterator = curry(iterator);
-
-  Object.defineProperty(reversedIterator, SIGNATURES.reversedIterator, {
-    value: true,
-    configurable: false,
-    enumerable: false,
-    writable: false,
-  });
-
-  return reversedIterator;
+  return assignSignature('reversedIterator', true, reversedIterator);
 };
