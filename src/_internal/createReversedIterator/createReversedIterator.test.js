0cd8d8e45a6efb81c61a4150e8f41c0c1baf1215
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 14:23:00 +0300

    use constant signatures in createReverseIterator tests

--- src/_internal/createReversedIterator/createReversedIterator.test.js
+++ src/_internal/createReversedIterator/createReversedIterator.test.js
@@ -1,12 +1,13 @@
 const test = require('tape-catch');
 const testUtils = require('../../../testUtils');
 const createReversedIterator = require('./createReversedIterator');
+const SIGNATURES = require('../signatures');
 
 test('_internal/createReversedIterator', (t) => {
   testUtils.isCurried(t, createReversedIterator((a, b) => a + b)(1), 'produces curried function');
 
   t.true(
-    createReversedIterator((a, b) => a + b)['@@nier/reversedIterator'],
+    createReversedIterator((a, b) => a + b)[SIGNATURES.reversedIterator],
     'creates reversedIterator static property on curried function'
   );
 
