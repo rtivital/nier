b93abfaba5cfb460339a1fa4bee0d8b7f1e7adba
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 12:31:56 +0300

    add withIndex test to findIndex module

--- src/findIndex/findIndex.test.js
+++ src/findIndex/findIndex.test.js
@@ -5,6 +5,7 @@ const testUtils = require('../../testUtils');
 test('findIndex', (t) => {
   testUtils.isCurried(t, findIndex(val => val > 2));
   testUtils.noIndex(t, { fn: findIndex, data: [1, 2, 3], result: 0 });
+  testUtils.withIndex(t, { fn: findIndex, data: [1, 2], indexResult: 1, dataResult: 0 });
 
   t.equals(
     findIndex(val => val > 2, [0, 1, 2, 3, 4]),
