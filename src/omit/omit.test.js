3096ec119a73cc363dbc4274b9e8f831caef5fce
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-20 11:39:17 +0300

    add omit tests

---
+++ src/omit/omit.test.js
@@ -0,0 +1,21 @@
+const test = require('tape-catch');
+const testUtils = require('../../testUtils');
+const omit = require('./omit');
+
+test.only('omit', (t) => {
+  testUtils.isCurried(t, omit(['a', 'b']));
+
+  t.deepEquals(
+    omit(['a', 'b'], { a: 1, b: 2, c: 3 }),
+    { c: 3 },
+    'removes specified keys from object'
+  );
+
+  t.deepEquals(
+    omit(['a', 'd'], { a: 1, b: 2 }),
+    { b: 2 },
+    'removes only keys that exist in object'
+  );
+
+  t.end();
+});
