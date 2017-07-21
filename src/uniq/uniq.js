f4015517b574912ef1aa448fab33bcc839b0d7ed
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-21 16:43:04 +0300

    add uniq docs

--- src/uniq/uniq.js
+++ src/uniq/uniq.js
@@ -1,6 +1,22 @@
 const curry = require('../curry/curry');
 const includes = require('../includes/includes');
 
+
+/**
+ * Creates new array that contains all the same items from the initial one,
+ * but all duplicates are excluded.
+ *
+ * @since v1.0.0
+ * @category Array
+ *
+ * @param {Array} array
+ * @return {Array}
+ *
+ * @example
+ * N.uniq([1, 1, 1, 2, 3]); // -> [1, 2, 3]
+ * N.uniq([{ a: 1 }, { b: 2 }, { a: 1 }]); // -> [{ a: 1 }, { b: 2 }]
+ * N.uniq([1, 2, 3]); // -> [1, 2, 3]
+ */
 function uniq(array) {
   const result = [];
 
