e22dbfc3d07299382d547e9ee9f53bf7e9429877
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-21 12:07:53 +0300

    add fillWith tests

---
+++ src/fillWith/fillWith.test.js
@@ -0,0 +1,25 @@
+const test = require('tape-catch');
+const testUtils = require('../../testUtils');
+const fillWith = require('./fillWith');
+
+test('fillWith', (t) => {
+  testUtils.isCurried(t, fillWith(5));
+
+  t.throws(
+    () => fillWith(null, () => 5),
+    'throws if provided length is not a valid number'
+  );
+
+  t.throws(
+    () => fillWith(5, null),
+    'throws if provided callback is not a function'
+  );
+
+  t.deepEquals(
+    fillWith(3, f => f),
+    [0, 1, 2],
+    'provides index to callback'
+  );
+
+  t.end();
+});
