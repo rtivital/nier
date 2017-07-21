d6e3016ac09df957aa5b4edc2a24584678199b7d
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-21 12:28:30 +0300

    fix prepend docs

--- src/prepend/prepend.js
+++ src/prepend/prepend.js
@@ -4,7 +4,7 @@ const curry = require('../curry/curry');
 /**
  * Creates new array containing the contents of the given array or array like structure,
  * with the give value at the front, folowed by the content of the array.
- * Note that prepend will create only a shallow copy of provided data structure.
+ * Note that all array like data structures (e.g. strings, arguments) will be automaticly transformed to array.
  *
  * @since v1.0.0
  * @category Array
@@ -13,7 +13,7 @@ const curry = require('../curry/curry');
  * @param {Array} data array to add element to
  * @return {Array} new array with the provided value in front
  *
- * @see prepend, concat
+ * @see append, concat
  *
  * @example
  * N.prepend('nier', ['hello']); // -> ['nier', 'hello']
