f00cfc6d3e37a1ee9512ec5fc9e300b27119d937
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-23 13:13:21 +0300

    add merge docs

--- src/merge/merge.js
+++ src/merge/merge.js
@@ -5,6 +5,23 @@ const reduce = require('../reduce/reduce');
 const has = require('../has/has');
 
 
+/**
+ * Creates new object with properties of the first object combined with properties of the second object.
+ * If a key exists in both objects, value from the second object will be assigned.
+ *
+ * @since v1.0.0
+ * @category Object
+ *
+ * @param {Object} a first object
+ * @param {Object} b second object
+ * @return {Object} result of merging a and b
+ *
+ * @alias assign
+ *
+ * @example
+ * N.merge({ a: 1 }, { b: 2 }); // -> { a: 1, b: 2 }
+ * N.merge({ a: 1, b: 3 }, { a: 2 }); // -> { a: 2, b: 3 }
+ */
 function merge(a, b) {
   const resultKeys = uniq([...keys(a), ...keys(b)]);
 
