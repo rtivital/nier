a1038fa3a638b45356241164c0e84ca37cd5666b
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-24 17:36:28 +0300

    add clone array like docs

--- src/_internal/cloneArrayLike/cloneArrayLike.js
+++ src/_internal/cloneArrayLike/cloneArrayLike.js
@@ -1,3 +1,17 @@
+
+/**
+ * Returns new array created from array or array like data structure.
+ *
+ * @since v1.0.0
+ *
+ * @param {*} array value to clone
+ * @return {Array}
+ *
+ * @example
+ * cloneArrayLike([1, 2, 3]); // -> [1, 2, 3]
+ * cloneArrayLike({ 0: 'a', 1: 'b', length: 2 }); // -> ['a', 'b']
+ * cloneArrayLike('nier'); // -> ['n', 'i', 'e', 'r']
+ */
 function cloneArrayLike(array) {
   const result = [];
 
