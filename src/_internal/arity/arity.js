1763d4c4ea270722762bc4a01767aadf5cefc510
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-24 13:29:53 +0300

    add arity docs

--- src/_internal/arity/arity.js
+++ src/_internal/arity/arity.js
@@ -1,5 +1,24 @@
 /* eslint-disable no-new-func */
 
+
+/**
+ * Internal – this function is not available at public api.
+ *
+ * Creates new function with provided length value.
+ * Although produced function length is fixed it can accept any amount of arguments.
+ *
+ * @since v1.0.0
+ * @category Function
+ *
+ * @param {number} length
+ * @param {Function} fn
+ * @return {Function}
+ *
+ * @example
+ * arity(5, f => f).length; // -> 5
+ * arity(2, (...args) => args[0] + args[1]).length; // -> 2
+ * arity(50, (..args) => args.reduce((acc, item) => acc + item)).length; // -> 50
+ */
 function arity(length, fn) {
   const params = [];
 
