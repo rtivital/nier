ed3a223a581b723d27ff763541c420590ddf0e1d
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-24 13:36:47 +0300

    fix broken assignSignature name

--- src/_internal/assignSignature/assignSignature.js
+++ src/_internal/assignSignature/assignSignature.js
@@ -1,6 +1,6 @@
 const SIGNATURES = require('../signatures');
 
-module.exports = function assignSignature(signature, value, object) {
+function assignSignature(signature, value, object) {
   Object.defineProperty(object, SIGNATURES[signature], {
     value,
     configurable: false,
@@ -9,4 +9,6 @@ module.exports = function assignSignature(signature, value, object) {
   });
 
   return object;
-};
+}
+
+module.exports = assignSignature;
