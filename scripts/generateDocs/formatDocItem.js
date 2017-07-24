99b8de0f2174245db74dad09ff81232ceddc9f0f
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-24 13:03:12 +0300

    remove all function declarations from doc item formatting

--- scripts/generateDocs/formatDocItem.js
+++ scripts/generateDocs/formatDocItem.js
@@ -1,23 +1,18 @@
 const N = require('../../src');
 const extractTagContent = require('./extractTagContent');
 
-const extractDescription = N.path(['description', 'full']);
-const extractName = N.path(['ctx', 'name']);
-
-const extractParams = N.pipe(
-  N.path('tags'),
-  N.filter(N.whereEq({ type: 'param' })),
-  N.map(N.pick(['name', 'description', 'typesDescription']))
-);
-
 module.exports = function format(docItem) {
   return {
-    name: extractName(docItem),
+    name: N.path(['ctx', 'name'], docItem),
     since: extractTagContent('since')(docItem),
     see: extractTagContent('see')(docItem),
     category: extractTagContent('category')(docItem),
     examples: extractTagContent('example')(docItem),
-    description: extractDescription(docItem),
-    params: extractParams(docItem),
+    description: N.path(['description', 'full'], docItem),
+    params: N.pipe(
+      N.path('tags'),
+      N.filter(N.whereEq({ type: 'param' })),
+      N.map(N.pick(['name', 'description', 'typesDescription']))
+    )(docItem),
   };
 };
