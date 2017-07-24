59b94b703d1c0bb552abcf4ace6f7280bbe71f5d
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-24 17:41:28 +0300

    add createReversedIterator docs

--- src/_internal/createReversedIterator/createReversedIterator.js
+++ src/_internal/createReversedIterator/createReversedIterator.js
@@ -1,7 +1,22 @@
 const curry = require('../../curry/curry');
 const assignSignature = require('../assignSignature/assignSignature');
 
-module.exports = function createReversedIterator(iterator) {
+/**
+ * Returns curried iterator with reversedIterator signature (used is withIndex function).
+ *
+ * @since v1.0.0
+ *
+ * @param {Function} iterator function that reversedIterator signature should be assigned to
+ * @return {Function}
+ *
+ * @example
+ * const findLastIterator = createReversedIterator(findLast); // -> findLast with hidden property
+ * isReversedIterator(findLastIterator); // -> true
+ * isReversedIterator(findLast); // -> false
+ */
+function createReversedIterator(iterator) {
   const reversedIterator = curry(iterator);
   return assignSignature('reversedIterator', true, reversedIterator);
-};
+}
+
+module.exports = createReversedIterator;
