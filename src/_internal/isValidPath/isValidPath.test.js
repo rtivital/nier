bc37cf6e1bc3a9fea9f618c107b53ed7768afdb6
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-25 17:32:25 +0300

    add number path validation tests to isValidPath module

--- src/_internal/isValidPath/isValidPath.test.js
+++ src/_internal/isValidPath/isValidPath.test.js
@@ -3,10 +3,11 @@ const isValidPath = require('./isValidPath');
 
 test('_internal/isValidPath', (t) => {
   t.true(isValidPath('hello'), 'string path is valid');
-
   t.true(isValidPath(['hello', 'nier']), 'array of strings path is valid');
-  t.false(isValidPath([]), 'empty array is invalid');
+  t.true(isValidPath(0), 'number path is valid');
+  t.true(isValidPath([0, 'hello', 1, 'there', 2, 'nier', 3]), 'combined string and number path is valid');
 
+  t.false(isValidPath([]), 'empty array is invalid');
   t.false(isValidPath(null), 'null path is invalid');
   t.false(isValidPath(Symbol('nier')), 'symbol path is invalid');
   t.false(isValidPath(false), 'boolean path is invalid');
