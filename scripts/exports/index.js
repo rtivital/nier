23e8185be551028f3b6b061cb790b63bb2458270
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-25 17:50:32 +0300

    add new exports script

--- scripts/exports/index.js
+++ scripts/exports/index.js
@@ -5,10 +5,9 @@ const getSrcContent = require('../utils/getSrcContent');
 const N = require('../../src');
 
 const exportedModules = N.keys(N);
-const title = chalk.blue.bold('allexports:');
 const getMissingModules = N.reject(N.flip(N.includes)(exportedModules));
 const getMissingMessages = N.pipe(
-  N.map(module => `${title} Missing module: ${chalk.red.bold(module)}`),
+  N.map(module => `Exports test: ✗ Missing module: ${chalk.red.bold(module)}`),
   N.join('\n')
 );
 
@@ -16,9 +15,10 @@ getSrcContent().then((modules) => {
   const missingModules = getMissingModules(modules);
 
   if (missingModules.length === 0) {
-    console.log(
-      `${title} No missing modules\n`
-    );
+    console.log(`
+      Exports test: ${chalk.green`✔ No missing modules!`}
+      Total modules: ${chalk.blue.bold(modules.length)}
+    `);
 
     process.exit(0);
   }
