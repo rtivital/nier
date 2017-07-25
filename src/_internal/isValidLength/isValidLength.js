89651afa4bc75ab8335748d5838c89e837fb62fb
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-25 13:24:39 +0300

    add isValidLength docs

--- src/_internal/isValidLength/isValidLength.js
+++ src/_internal/isValidLength/isValidLength.js
@@ -1,3 +1,23 @@
+/**
+ * Internal – this function is not available in public api.
+ *
+ * Returns true if provided value can be valid length of iterable data structure.
+ *
+ * @since v1.0.0
+ * @category Internal
+ *
+ * @param {*} length
+ * @return {boolean}
+ *
+ * @example
+ * isValidLength(10); // -> true
+ * isValidLength(0); // -> true
+ *
+ * isValidLength(-1); // -> false
+ * isValidLength(0.6); // -> false
+ * isValidLength(Number.MAX_SAFE_INTEGER + 1); // -> false
+ * isValidLength('1'); // -> false
+ */
 function isValidLength(length) {
   return (
     typeof length === 'number'
