9e8b963caa858a8c589c66996b9afebee51f576b
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-25 17:30:37 +0300

    add isValidPath module docs

--- src/_internal/isValidPath/isValidPath.js
+++ src/_internal/isValidPath/isValidPath.js
@@ -1,4 +1,26 @@
-module.exports = function isValidPath(path) {
+/**
+ * Returns true if provided value is string, number or Array of strings or numbers.
+ *
+ * @since v1.0.0
+ * @category Internal
+ *
+ * @param {*} path
+ * @return {boolean}
+ *
+ * @example
+ * isValidPath('hello'); // -> true
+ * isValidPath([0, 'hello', 1, 'there', 2, 'nier', 3]); // -> true
+ * isValidPath(0); // -> true
+ *
+ * isValidPath([]); // -> false
+ * isValidPath(null); // -> false
+ * isValidPath(Symbol('nier')); // -> false
+ * isValidPath(false); // -> false
+ * isValidPath(undefined); // -> false
+ * isValidPath({}); // -> false
+ * isValidPath(f => f); // -> false
+ */
+function isValidPath(path) {
   const pathType = typeof path;
 
   if (pathType === 'string' || pathType === 'number') {
@@ -24,4 +46,6 @@ module.exports = function isValidPath(path) {
   }
 
   return false;
-};
+}
+
+module.exports = isValidPath;
