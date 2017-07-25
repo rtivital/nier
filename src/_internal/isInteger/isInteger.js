15da9935f0b8cac154038ed120659b4d9068d2db
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-25 13:35:46 +0300

    add isInteger docs

--- src/_internal/isInteger/isInteger.js
+++ src/_internal/isInteger/isInteger.js
@@ -1,3 +1,19 @@
+/**
+ * Internal – this function is not available in public api.
+ *
+ * Returns true if provided value is an integer.
+ *
+ * @since v1.0.0
+ * @category Internal
+ *
+ * @param {*} value
+ * @return {boolean}
+ *
+ * @example
+ * isInteger(1); // -> true
+ * isInteger(1.5); // -> false
+ * isInteger('1.5'); // -> false
+ */
 function isInteger(value) {
   return typeof value === 'number' && value % 1 === 0;
 }
