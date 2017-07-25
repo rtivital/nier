3bba835f38757333dffdcd629afb6c679b122596
Vitaly Rtishchev <rtivital@gmail.com>
2017-07-25 12:04:48 +0300

    add curryN docs

--- src/_internal/curryN/curryN.js
+++ src/_internal/curryN/curryN.js
@@ -1,5 +1,24 @@
 const arity = require('../arity/arity');
 
+
+/**
+ * Creates currried function with provided length.
+ *
+ * @since v1.0.0
+ * @category Internal
+ *
+ * @param {number} receivedArgs length of function that should be returned
+ * @param {Array} acc arguments accumulator
+ * @param {Function} fn function that should be curried
+ * @return {Function} curried function
+ *
+ * @example
+ * const curried = curryN(2, [], (...args) => args[0] + args[1]);
+ * curried.length; //-> 2
+ * curried(1).length; // -> 1
+ * curried()()()().length; // -> 2
+ * curried('hello', ' there'); // -> 'hello there'
+ */
 function curryN(receivedArgs, acc, fn) {
   const callsRemain = receivedArgs || fn.length;
 
