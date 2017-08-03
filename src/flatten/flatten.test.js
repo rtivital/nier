6c784f29515c7670a2e394d5f83f082a1f76f001
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-03 11:47:26 +0300

    add flatten tests

---
+++ src/flatten/flatten.test.js
@@ -0,0 +1,27 @@
+const test = require('tape-catch');
+const testUtils = require('../../testUtils');
+const flatten = require('./flatten');
+
+test('flatten', (t) => {
+  testUtils.isCurried(t, flatten());
+
+  t.deepEquals(
+    flatten([1, [2], 3]),
+    [1, 2, 3],
+    'flattens array with single level of nesting'
+  );
+
+  t.deepEquals(
+    flatten([[[[[1], 2]]], [3], 4, [5, [6, 7]]]),
+    [1, 2, 3, 4, 5, 6, 7],
+    'flattens array with multiple levels of nesting'
+  );
+
+  t.deepEquals(
+    flatten([1, 2, 3]),
+    [1, 2, 3],
+    'does not modify already flat arrays'
+  );
+
+  t.end();
+});
