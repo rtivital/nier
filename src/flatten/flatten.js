bcc4542882fd7d44ba846e5afaf229d9dcd69e59
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-03 11:56:37 +0300

    add flatten docs

--- src/flatten/flatten.js
+++ src/flatten/flatten.js
@@ -1,5 +1,22 @@
 const curry = require('../curry/curry');
 
+
+/**
+ * Recursively removes nesting from array.
+ *
+ * @since v1.0.0
+ * @category Array
+ *
+ * @param {Array} list
+ * @return {Array}
+ *
+ * @see unnest
+ *
+ * @example
+ * N.flatten([1, [2], 3]); // -> [1, 2, 3]
+ * N.flatten([[[[[1], 2]]], [3], 4, [5, [6, 7]]]); // -> [1, 2, 3, 4, 5, 6, 7]
+ * N.flatten([1, 2, 3]); // -> [1, 2, 3]
+ */
 function flatten(list) {
   const result = [];
 
