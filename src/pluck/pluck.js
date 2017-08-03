3b804de6e44dbd7fe4ff0236d9679e01b796e1f4
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-03 12:38:13 +0300

    add array check to pluck

--- src/pluck/pluck.js
+++ src/pluck/pluck.js
@@ -3,6 +3,10 @@ const map = require('../map/map');
 const path = require('../path/path');
 
 function pluck(propPath, list) {
+  if (!Array.isArray(list)) {
+    throw new Error('N.pluck received non array value');
+  }
+
   return map(path(propPath), list);
 }
 
