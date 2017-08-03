771ad66fd651f4535097d650fa5cd049a00fa561
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-03 12:40:51 +0300

    add pluck path throwings

--- src/pluck/pluck.js
+++ src/pluck/pluck.js
@@ -1,8 +1,13 @@
+const isValidPath = require('../_internal/isValidPath/isValidPath');
 const curry = require('../curry/curry');
 const map = require('../map/map');
 const path = require('../path/path');
 
 function pluck(propPath, list) {
+  if (!isValidPath(propPath)) {
+    throw new Error('N.pluck received invalid property path');
+  }
+
   if (!Array.isArray(list)) {
     throw new Error('N.pluck received non array value');
   }
