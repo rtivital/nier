cd627b20e8006bd2f8fba3106e3e776032391f10
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-05 13:44:36 +0300

    setup webpack build

---
+++ scripts/docs/start.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const chalk = require('chalk');
+const webpackConfig = require('../../docs/webpack.config');
+const SETTINGS = require('../../docs/settings');
+
+const serverConfig = {
+  contentBase: './public',
+  publicPath: '/',
+  stats: { colors: true },
+  hot: true,
+  historyApiFallback: true,
+};
+
+new WebpackDevServer(webpack(webpackConfig), serverConfig)
+  .listen(SETTINGS.PORT, 'localhost', (error) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`
+        Listening at ${chalk.bold.cyan(`http://localhost:${SETTINGS.PORT}/`)}.
+        Browser will be opened automaticly when webpack finish building.
+      `);
+    }
+  });
