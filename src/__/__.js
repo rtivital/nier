ab1be51a0dadd642ca9b6a41dcfc5870b8b45b10
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-05 14:34:58 +0300

    fix placeholder breaking build examples

--- src/__/__.js
+++ src/__/__.js
@@ -1,5 +1,12 @@
 const assignSignature = require('../_internal/assignSignature/assignSignature');
 
+/**
+ * Placeholder
+ *
+ * @example
+ * const call = N.call(N.__, 1, 2);
+ * call((a, b) => a + b); // -> 3
+ */
 const __ = {};
 
 module.exports = assignSignature('placeholder', true, __);
