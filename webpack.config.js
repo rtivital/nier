c9f9a86d41a99072cd4c5fc95b970c484d95f7f6
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-08 18:32:26 +0300

    remove needless array webpack entry wrapping

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: ['./src/index'],
+  entry: './src/index',
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'index.js',
