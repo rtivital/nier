616f922ef24eb2e638bc4fd2188dc4c32d2b7677
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-08 12:49:53 +0300

    add new home page text

--- docs/src/components/routes/home/HomePage.jsx
+++ docs/src/components/routes/home/HomePage.jsx
@@ -5,11 +5,11 @@ export default function HomePage() {
   return (
     <div>
       <h1 style={{ fontSize: 50, fontWeigth: 'bold', textAlign: 'center' }}>
-        React Pages Boilerplate
+        Nier.js website
       </h1>
 
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Button theme="blue">Click me!!</Button>
+        <Button theme="blue">Click me!</Button>
       </div>
     </div>
   );
