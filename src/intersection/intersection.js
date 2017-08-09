4edc772c8b554ea689c90da262df3ae122d9807c
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-09 12:09:42 +0300

    move intersection type checks back to arrays

--- src/intersection/intersection.js
+++ src/intersection/intersection.js
@@ -1,14 +1,13 @@
-const _isArrayLike = require('../_internal/isArrayLike/isArrayLike');
 const curry = require('../curry/curry');
 const includes = require('../includes/includes');
 
 function intersection(a, b) {
-  if (!_isArrayLike(a)) {
-    throw new Error('N.intersection received first argument that is not an array or array like data structure');
+  if (!Array.isArray(a)) {
+    throw new Error('N.intersection received first argument that is not an array');
   }
 
-  if (!_isArrayLike(a)) {
-    throw new Error('N.intersection received second argument that is not an array or array like data structure');
+  if (!Array.isArray(a)) {
+    throw new Error('N.intersection received second argument that is not an array');
   }
 
   const result = [];
