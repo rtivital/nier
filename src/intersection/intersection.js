1d3b44bf38a4e503db543a4f20b312e4f50a1ea5
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-09 13:11:37 +0300

    add intersection docs

--- src/intersection/intersection.js
+++ src/intersection/intersection.js
@@ -2,6 +2,25 @@ const curry = require('../curry/curry');
 const includes = require('../includes/includes');
 const uniq = require('../uniq/uniq');
 
+
+/**
+ * Combines two arrays into a new array that contains only elements that are included in both arrays.
+ * The result array will not contain duplicates and will preserve items order of first array.
+ *
+ * @since v1.0.0
+ * @category Array
+ *
+ * @param {Array} a
+ * @param {Array} b
+ * @return {Array}
+ *
+ * @example
+ * N.intersection([1, 2, 3], [2, 3, 4]); // -> [2, 3]
+ * N.intersection([1, 2, 3], [3, 2, 1]); // -> [1, 2, 3]
+ * N.intersection([1, 1, 2, 2], [1, 1, 2, 2, 3]); // -> [1, 2]
+ * N.intersection([1, 2, 3], [4, 5, 6]); // -> []
+ * N.intersection([{ a: 1 }, { a: 2 }, { a: 3 }], [{ a: 2 }, { a: 3 }, { a: 4 }]); // -> [{ a: 2 }, { a: 3 }]
+ */
 function intersection(a, b) {
   if (!Array.isArray(a)) {
     throw new Error('N.intersection received first argument that is not an array');
