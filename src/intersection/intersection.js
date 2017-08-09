f0ab6ebb09bf8fb6bf887c4dcbca56479fddd25c
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-09 12:02:37 +0300

    add array like checks to intersection

--- src/intersection/intersection.js
+++ src/intersection/intersection.js
@@ -1,7 +1,16 @@
+const _isArrayLike = require('../_internal/isArrayLike/isArrayLike');
 const curry = require('../curry/curry');
 const includes = require('../includes/includes');
 
 function intersection(a, b) {
+  if (!_isArrayLike(a)) {
+    throw new Error('N.intersection received first argument that is not an array or array like data structure');
+  }
+
+  if (!_isArrayLike(a)) {
+    throw new Error('N.intersection received second argument that is not an array or array like data structure');
+  }
+
   const result = [];
 
   for (let i = 0, l = a.length; i < l; i += 1) {
