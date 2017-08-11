a40fe3a24ba57d0b7f0979e1fc377ef309ffd8ce
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-11 12:53:36 +0300

    move eslint configuration to js file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: 'airbnb-base',
+  plugins: ['filenames'],
+  rules: {
+    'filenames/match-exported': 'error',
+    'filenames/no-index': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'error',
+    'global-require': 'off',
+    'max-len': ['error', { code: 120, ignoreStrings: true, ignoreComments: true }],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+  },
+}
