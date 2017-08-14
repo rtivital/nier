8dcad8ac3e5af996312cd58fdec421983f22e073
Vitaly Rtishchev <rtivital@gmail.com>
2017-08-14 17:02:09 +0300

    add new pipe tests

--- src/pipe/pipe.test.js
+++ src/pipe/pipe.test.js
@@ -10,40 +10,29 @@ const upperLast = str => `${str.slice(0, str.length - 1)}${str.slice(str.length 
 const undefinedToZero = value => (typeof value === 'undefined' ? 0 : value);
 const getUndefined = () => undefined;
 
-const pipeSingleArgument = pipe(
-  mutiplyBy2, // will be called first
-  add1, // will be called second
-  divideBy3 // will be called last
-); // equals to –> (value) => mutiplyBy2(add1(divideBy3(value)))
-
-const pipeDoubleArgument = pipe(
-  concat, // will be called first
-  upperFirst, // will be called second
-  upperLast // will be called last
-); // equals to –> (value) => divideBy3(add1(mutiplyBy2(value)))
-
-const pipeWithFalsyReturn = pipe(
-  getUndefined, // will be called first
-  undefinedToZero, // will be called second
-  add1 // will be called last
-); // equals to –> () => add1(undefinedToZero(getUndefined()))
-
 test('pipe', (t) => {
-  const singleArgumentTest = divideBy3(add1(mutiplyBy2(1)));
-  const doubleArgumentTest = upperFirst(upperLast(concat('hello', 'there')));
-  const falsyReturnTest = add1(undefinedToZero(getUndefined()));
+  t.throws(
+    () => pipe(),
+    'throws if argument is not passed'
+  );
 
-  // pipe functions should throw an error if it has not received any arguments
-  t.throws(() => pipe(), 'pipe throws error if argument is not passed');
+  t.equals(
+    pipe(mutiplyBy2, add1, divideBy3)(1),
+    divideBy3(add1(mutiplyBy2(1))),
+    'works with function that accept single argument'
+  );
 
-  // function created with pipe should work with functions that accept only one argument
-  t.equals(pipeSingleArgument(1), singleArgumentTest, 'pipe works with single argument functions');
+  t.equals(
+    pipe(concat, upperFirst, upperLast)('hello', 'there'),
+    upperFirst(upperLast(concat('hello', 'there'))),
+    'works with function that accept multiple arguments'
+  );
 
-  // function created with pipe should work with functions that accept multiple arguments
-  t.equals(pipeDoubleArgument('hello', 'there'), doubleArgumentTest, 'pipe works with functions that accept multiple arguments');
-
-  // pipe should be able to handle functions that return falsy values
-  t.equals(pipeWithFalsyReturn(), falsyReturnTest, 'pipe works with function that return falsy values');
+  t.equals(
+    pipe(getUndefined, undefinedToZero, add1)(),
+    add1(undefinedToZero(getUndefined())),
+    'works with function that return falsy values'
+  );
 
   t.end();
 });
