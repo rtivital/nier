13cd568acccb24e9ce94b90a2837186eba9b2970
Vitaly Rtishchev <rtivital@gmail.com>
2018-06-13 14:55:50 +0300

    add new throwing tests to arity

--- src/_internal/arity/arity.test.js
+++ src/_internal/arity/arity.test.js
@@ -4,23 +4,12 @@ const arity = require('./arity');
 const sum = (...args) => args.reduce((acc, item) => acc + item, 0);
 
 test('_internal/arity', (t) => {
-  t.equals(
-    arity(1, sum).length,
-    1,
-    'provides arity to function'
-  );
-
-  t.equals(
-    arity(34, sum).length,
-    34,
-    'provides arity to function'
-  );
-
-  t.equals(
-    arity(2, sum)(1, 2, 3, 4),
-    10,
-    'returned function works as regular'
-  );
+  t.equals(arity(1, sum).length, 1, 'provides arity to function');
+  t.equals(arity(34, sum).length, 34, 'provides arity to function');
+  t.equals(arity(2, sum)(1, 2, 3, 4), 10, 'returned function works as regular');
+  t.throws(() => arity('string length', sum)(1, 2, 3), 'throws with string length');
+  t.throws(() => arity(null, sum)(1, 2, 3), 'throws with null length');
+  t.throws(() => arity(-1, sum)(1, 2, 3), 'throws with negative length');
 
   t.end();
 });
