ace9ec427bc9f497b12912c2515d4005b97f0e00
Vitaly Rtishchev <rtivital@gmail.com>
2018-06-14 17:36:30 +0300

    add validate arguments with function

---
+++ src/_internal/validateArgumentWith/validateArgumentWith.js
@@ -0,0 +1,9 @@
+const ArgumentError = require('../ArgumentError/ArgumentError');
+
+function validateArgumentWith(functionName, argumentPosition, predicate, value, message) {
+  if (!predicate(value)) {
+    throw new ArgumentError(`${functionName} expected argument at position \`${argumentPosition}\` to ${message}`);
+  }
+}
+
+module.exports = validateArgumentWith;
