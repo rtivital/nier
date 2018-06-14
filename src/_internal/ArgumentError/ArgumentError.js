e908583845ba7deb5baa221d38e00605fae109db
Vitaly Rtishchev <rtivital@gmail.com>
2018-06-14 16:35:55 +0300

    add argument error

--- src/_internal/ArgumentError/ArgumentError.js
+++ src/_internal/ArgumentError/ArgumentError.js
@@ -1,3 +1,27 @@
+/**
+ * Internal – this class is not available in public api.
+ *
+ * Creates all argument related errors.
+ *
+ * @since v1.0.0
+ * @category Internal
+ *
+ * @param {string} functionName name of function at which error occured
+ * @param {number} argumentPosition position of tested argument
+ * @param {string} expectedType expected argument type
+ * @param {*} valueType argument value type
+ *
+ * @example
+ * const argumentError = new ArgumentError({
+ *   functionName: 'N._internal/ArgumentError',
+ *   argumentPosition: 1,
+ *   expectedType: 'String',
+ *   valueType: 'Number',
+ * });
+ *
+ * argumentError.message; // -> 'N._internal/ArgumentError expected argument at position `1` to have type: `String`, instead got: `Number`'
+ * argumentError.toString(); // -> 'ArgumentError: N._internal/ArgumentError expected argument at position `1` to have type: `String`, instead got: `Number`'
+ */
 class ArgumentError extends Error {
   constructor({ functionName, argumentPosition, expectedType, valueType }) {
     const message = `${functionName} expected argument at position \`${argumentPosition}\` to have type: \`${expectedType}\`, instead got: \`${valueType}\``;
