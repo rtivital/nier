fd87ce72fe545ad8b30fa2f2538c03a0c8c74415
Vitaly Rtishchev <rtivital@gmail.com>
2018-06-14 12:46:53 +0300

    add better docs to without

--- src/without/without.js
+++ src/without/without.js
@@ -2,17 +2,17 @@ const curry = require('../curry/curry');
 const includes = require('../includes/includes');
 
 /**
- * Returns new array without values specified in the first argument.
+ * Returns new array without values specified in the first argument. N.equals is used to determine equality.
  *
  * @since v1.0.0
  * @category Array
  *
- * @param {Array} exclude
- * @param {Array} list
+ * @param {Array} exclude array of values that will be excluded from the array
+ * @param {Array} list array from which values will be excluded
  * @return {Array}
  *
  * @example
- * N.without([1, 2, 3], [1, 2, 3, 4, 5, 6]); // -> [4, 5, 6]
+ * N.without([1, 2, 3], [1, 1, 2, 2, 2, 3, 4, 5, 6]); // -> [4, 5, 6]
  * N.without([{ a: 1 }, { b: 2 }], [{ a : 1 }, { b: 2 }, { c: 3 }, { d: 4 }]); // -> [{ c: 3 }, { d: 4 }]
  */
 function without(exclude, list) {
