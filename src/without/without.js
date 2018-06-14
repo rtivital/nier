242c4228431130ec77ab9e83a7e1d604bf105092
Vitaly Rtishchev <rtivital@gmail.com>
2018-06-14 12:51:10 +0300

    rewrite without with reject

--- src/without/without.js
+++ src/without/without.js
@@ -1,5 +1,7 @@
 const curry = require('../curry/curry');
 const includes = require('../includes/includes');
+const reject = require('../reject/reject');
+const flip = require('../flip/flip');
 
 /**
  * Returns new array without values specified in the first argument. N.equals is used to determine equality.
@@ -7,32 +9,24 @@ const includes = require('../includes/includes');
  * @since v1.0.0
  * @category Array
  *
- * @param {Array} exclude array of values that will be excluded from the array
- * @param {Array} list array from which values will be excluded
+ * @param {Array} valuesToExclude array of values that will be excluded from the array
+ * @param {Array} array array from which values will be excluded
  * @return {Array}
  *
  * @example
  * N.without([1, 2, 3], [1, 1, 2, 2, 2, 3, 4, 5, 6]); // -> [4, 5, 6]
  * N.without([{ a: 1 }, { b: 2 }], [{ a : 1 }, { b: 2 }, { c: 3 }, { d: 4 }]); // -> [{ c: 3 }, { d: 4 }]
  */
-function without(exclude, list) {
-  if (!Array.isArray(exclude)) {
+function without(valuesToExclude, array) {
+  if (!Array.isArray(valuesToExclude)) {
     throw new Error('N.without received non array first argument');
   }
 
-  if (!Array.isArray(list)) {
+  if (!Array.isArray(array)) {
     throw new Error('N.without received non array second argument');
   }
 
-  const result = [];
-
-  for (let i = 0, l = list.length; i < l; i += 1) {
-    if (!includes(list[i], exclude)) {
-      result.push(list[i]);
-    }
-  }
-
-  return result;
+  return reject(flip(includes)(valuesToExclude), array);
 }
 
 module.exports = curry(without);
